84979ce49b89324b691bf21c54a97bdaa2adbfec
avwo <avwu@qq.com>
2015-04-24 11:19:15 +0800

    install dependencies

--- tianma.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\tianma\bin\tianma" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\tianma\bin\tianma" %*
-)
