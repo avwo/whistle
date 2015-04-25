b720136ea8e783052b480530b49afd4f42af3b07
avwo <avwu@qq.com>
2015-04-25 13:43:57 +0800

    https proxy

--- util/index.js
+++ util/index.js
@@ -26,6 +26,7 @@ exports.config = util._extend({}, config);
 exports.argvs = require('./argvs');
 exports.WhistleTransform = require('./whistle-transform');
 exports.getNpm = getNpm;
+exports.proxyHttps = require('./https-proxy');
 
 exports.installTianma = function(callback) {
 	if (!callback && fs.existsSync(path.join(NODE_MODULES_PATH, 'tianma')) 
