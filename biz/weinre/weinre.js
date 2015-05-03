2386937b14c46a2afaafbd29e4f5abc4b99599fd
avwo <avwu@qq.com>
2015-05-03 11:31:17 +0800

    add weinre

--- biz/weinre/weinre.js
+++ biz/weinre/weinre.js
@@ -2,5 +2,9 @@ var config = require('../../util').config;
 var weinre = require('weinre');
 weinre.run({
 	boundHost: process.argv[3],
-	httpPort: process.argv[5]
+	httpPort: parseInt(process.argv[5]),
+	verbose: false,
+	debug: false,
+	readTimeout: 5,
+	deathTimeout: 15
 });
