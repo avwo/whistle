f29101af31d52c6f25a1de118e36ceb15fe2e0b4
avwo <avwu@qq.com>
2015-05-03 10:36:19 +0800

    add weinre

--- biz/weinre/app.js
+++ biz/weinre/app.js
@@ -1,6 +1,9 @@
 var cp = require('child_process');
 var path = require('path');
+var util = require('../../util');
+var config = util.config;
 
-cp.spawn('node', [path.join(__dirname, 'weinre.js')], {
+cp.spawn('node', [path.join(__dirname, 'weinre.js'), '--boundHost', 
+                  config.WEINRE_HOST, '--httpPort', config.weinreport], {
 	stdio: [ 0, 1, 2 ]
 });
--- biz/weinre/weinre.js
+++ biz/weinre/weinre.js
@@ -1,7 +1,6 @@
 var config = require('../../util').config;
 var weinre = require('weinre');
-
 weinre.run({
-	boundHost: config.WEINRE_HOST,
-	httpPort: config.weinreport
+	boundHost: process.argv[3],
+	httpPort: process.argv[5]
 });
