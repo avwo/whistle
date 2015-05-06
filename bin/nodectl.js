d3b368ac13ca0ea239ae4012120114f56030335f
avwo <avwu@qq.com>
2015-05-06 10:29:08 +0800

    all in a process

--- bin/nodectl.js
+++ bin/nodectl.js
@@ -83,7 +83,7 @@ exports.restart = restart;
  */
 
 function run(options) {
-	console.log('[i] Press [Ctrl+C] to stop ' + config.name + '.., port: ' + (options.port || config.port));
+	console.log('[i] Press [Ctrl+C] to stop ' + config.name + '.. port: ' + (options.port || config.port));
 	var args = [PATH_BOOTSTRAP, 'run', START_PATH];
 	options = setArgs(options, args);
 	
