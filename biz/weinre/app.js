c46efb2f9c97565b659c3df84215c7207ed0a140
avwo <avwu@qq.com>
2015-05-07 14:56:01 +0800

    shield the express@2.5.x __DefineGetter__

--- biz/weinre/app.js
+++ biz/weinre/app.js
@@ -1,8 +1,12 @@
 var config = require('../../util').config;
-var weinre = require('weinre');
 
 module.exports = function init(app) {
-	weinre.run({
+	//weinre用到老版本的express，改版本会直接往下面原型对象设置__defineGetter__
+	//会影响到同一进程的web应用，新版的express处理方式更加合理些
+	var req = require('http').IncomingMessage.prototype;
+	var __defineGetter__ = req.__defineGetter__;
+	req.__defineGetter__ = function() {};
+	require('weinre').run({
 		boundHost: 'localhost',
 		httpPort: parseInt(app.weinreport, 10),
 		verbose: false,
@@ -10,4 +14,5 @@ module.exports = function init(app) {
 		readTimeout: 5,
 		deathTimeout: 15
 	});
+	req.__defineGetter__ = __defineGetter__;
 };
