d46f047c86dee51267e4d89dbb52907070913189
avwo <avwu@qq.com>
2015-05-17 11:13:33 +0800

    add shortcut

---
+++ biz/webui/cgi-bin/init.js
@@ -0,0 +1,20 @@
+var rulesUtil = require('../lib/rules-util');
+var config = require('../lib/config');
+
+module.exports = function(req, res) {
+	var data = rulesUtil.getHostsData();
+	data.fontSize = rulesUtil.getProperty('fontSize');
+	data.showLineNumbers = rulesUtil.getProperty('showLineNumbers');
+	data.theme = rulesUtil.getProperty('theme');
+	data.version = config.version;
+	
+	res.json({
+		rules: data,
+		values: {
+			fontSize: rulesUtil.getProperty('valuesFontSize'),
+			theme: rulesUtil.getProperty('valuesTheme'),
+			showLineNumbers: rulesUtil.getProperty('valuesShowLineNumbers'),
+			values: rulesUtil.getValue()
+		}
+	});
+};
