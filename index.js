83fecaf26a3b6445be9b2aa4a10cc7ef24977185
avwo <avwu@qq.com>
2015-05-25 20:55:27 +0800

    exports whistle

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var start = require('./lib');
 var util = require('./util');
 
 module.exports = function init(options) {
 	if (options.dataDir) {
 		util.LOCAL_DATA_PATH = options.dataDir;
 	}
-	return start(options);
+	return require('./lib')(options);
 };
