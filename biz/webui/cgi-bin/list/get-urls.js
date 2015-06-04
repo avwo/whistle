577193a7f05a282e5c8f6093fe648f775bb054ea
av <aven@wuavendeMacBook-Pro.local>
2015-06-04 10:25:12 +0800

    -----------delete cache data---------------

--- biz/webui/cgi-bin/list/get-urls.js
+++ biz/webui/cgi-bin/list/get-urls.js
@@ -1,12 +1,7 @@
 var rulesUtil = require('../../lib/rules-util');
-var dataUtil = require('../../lib/data/util');
-
 
 module.exports = function(req, res) {
 	var query = req.query;
 	
-	dataUtil.getUrls(parseInt(query.startDate), parseInt(query.endDate), 
-			typeof query.keyword == 'string' ? query.keyword.trim() : '', function(list) {
-				res.json(list);
-	});
+	
 };
