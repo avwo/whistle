c3082d7689afe653000fa961b5119d54548bacbc
avwo <avwu@qq.com>
2015-07-14 12:36:57 +0800

    client ip

--- biz/webui/cgi-bin/composer/request.js
+++ biz/webui/cgi-bin/composer/request.js
@@ -58,7 +58,7 @@ module.exports = function(req, res) {
 			req.body.body = new Buffer(req.body.body || '');
 			headers['content-length'] = req.body.body.length;
 		}
-		headers['x-forwarded-for'] = req.ip;
+		headers['x-forwarded-for-' + config.name] = req.ip;
 		http.request(options, function(res) {
 			res.on('error', util.noop);
 			util.drain(res);
