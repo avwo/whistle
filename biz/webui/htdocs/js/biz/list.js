f7b94f23bcab74dcb58a2f55e0ccbbbf18c8cccc
avwo <avwu@qq.com>
2015-07-28 20:30:08 +0800

    remove prepend

--- biz/webui/htdocs/js/biz/list.js
+++ biz/webui/htdocs/js/biz/list.js
@@ -539,9 +539,6 @@ define('/style/js/biz/list.js', function(require, exports, module) {
 						Req: rules.req && rules.req.raw,
 						Rule: rules.rule && rules.rule.raw,
 						Res: rules.res && rules.res.raw,
-						Prepend: rules.prepend && rules.prepend.raw,
-						Body: rules.body && rules.body.raw,
-						Append: rules.append && rules.append.raw,
 						Weinre: rules.weinre && rules.weinre.raw,
 						Filter: rules.filter && rules.filter.raw
 					}));
