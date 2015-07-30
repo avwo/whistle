521ca81f949406a0d994562ea26e28fd22c1fca9
av <aven@wuavendeMacBook-Pro.local>
2015-07-30 09:19:00 +0800

    prevent weinre rewrite prepareStackTrace

--- biz/weinre/app.js
+++ biz/weinre/app.js
@@ -2,11 +2,13 @@
 module.exports = function init(config) {
 	//weinre用到老版本的express，改版本会直接往下面原型对象设置__defineGetter__
 	//会影响到同一进程的web应用，新版的express处理方式更加合理些
+	var noop = function() {};
+	var prepareStackTrace = Error.prepareStackTrace;
 	var req = require('http').IncomingMessage.prototype;
 	var __defineGetter__ = req.__defineGetter__;
-	req.__defineGetter__ = function() {};
+	req.__defineGetter__ = noop;
 	var log = console.log;
-	console.log = function() {};
+	console.log = noop;
 	require('weinre').run({
 		boundHost: 'localhost',
 		httpPort: parseInt(config.weinreport, 10),
@@ -17,4 +19,5 @@ module.exports = function init(config) {
 	});
 	req.__defineGetter__ = __defineGetter__;
 	console.log = log;
+	Error.prepareStackTrace = prepareStackTrace;
 };
