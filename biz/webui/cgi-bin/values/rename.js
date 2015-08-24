7181eca59991ae3d31527851e80fc7db4f8e0309
avwo <avwu@qq.com>
2015-08-25 00:11:52 +0800

    rename

--- biz/webui/cgi-bin/values/rename.js
+++ biz/webui/cgi-bin/values/rename.js
@@ -1,6 +1,6 @@
 var values = require('../../lib/values');
 
 module.exports = function(req, res) {
-	values.add(req.body.name, req.body.newName);
+	values.rename(req.body.name, req.body.newName);
 	res.json({ec: 0, em: 'success'});
 };
