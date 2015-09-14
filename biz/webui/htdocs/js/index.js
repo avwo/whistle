b412d2cc94724867ac93858abede7afbbf879cb9
avwo <avwu@qq.com>
2015-09-14 14:33:13 +0800

    add protocols

--- biz/webui/htdocs/js/index.js
+++ biz/webui/htdocs/js/index.js
@@ -45458,9 +45458,9 @@
 	 * [req, res]delay://, method://, [req, res][content]Type://自动lookup, 
 	 * cache://xxxs[no], params://json|string(放在url)
 	 */
-	var RULES = ['host', 'req', 'rule', 'res', 'weinre', 'filter', 'log', 'referer', 'auth', 'params', 'statusCode', 'redirect', 'method', 'cache', 
-	             'reqDelay', 'reqSpeed', 'reqType', 'reqHeaders', 'reqBody', 'prependReq', 'appendReq', 'resDelay', 'resSpeed', 
-	             'resType', 'resHeaders', 'resBody', 'prependRes', 'appendRes'];
+	var RULES = ['host', 'req', 'rule', 'res', 'weinre', 'filter', 'log', 'referer', 'auth', 'ua', 'params', 'statusCode', 'redirect', 'method', 'cache', 
+	             'reqDelay', 'reqSpeed', 'reqType', 'reqCookies', 'reqHeaders', 'reqBody', 'prependReq', 'appendReq', 'resDelay', 'resSpeed', 
+	             'resType', 'resCookies', 'resHeaders', 'resBody', 'prependRes', 'appendRes'];
 	var DEFAULT_OVERVIEW_MODAL = {};
 	var DEFAULT_RULES_MODAL = {};
 
