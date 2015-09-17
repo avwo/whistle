5182901ca664ef0021f73d5aa94d77ad3f1e5029
avwo <avwu@qq.com>
2015-09-17 14:39:59 +0800

    config

--- bin/whistle.js
+++ bin/whistle.js
@@ -2,7 +2,7 @@
 
 var program = require('../../starting');
 var path = require('path');
-var config = require('../package.json');
+var config = require('../lib/config');
 var util = require('../lib/util');
 var CONFIG_DIR = util.LOCAL_DATA_PATH;
 
