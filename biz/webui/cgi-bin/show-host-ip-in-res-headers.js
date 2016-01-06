bbcb03b753311fea0b5989d639fb1abc0fb9ec97
avwo <avwu@qq.com>
2016-01-06 13:39:10 +0800

    showHostIpInResHeaders

---
+++ biz/webui/cgi-bin/show-host-ip-in-res-headers.js
@@ -0,0 +1,6 @@
+var properties = require('../lib/properties');
+
+module.exports = function(req, res) {
+	properties.set('showHostIpInResHeaders', req.body.showHostIpInResHeaders == 1);
+	res.json({ec: 0, em: 'success'});
+};
