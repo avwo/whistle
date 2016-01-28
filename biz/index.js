5dbcca47c1a33e0e38632519cc67161dd778466d
avwo <avwu@qq.com>
2016-01-28 20:35:45 +0800

    add xxx.local.whitlejs.com

--- biz/index.js
+++ biz/index.js
@@ -6,6 +6,10 @@ var util = require('../lib/util');
 
 function request(req, res, port, weinre) {
 	var options = url.parse(util.getFullUrl(req));
+	if (options.protocol == 'https:') {
+		options.protocol = 'http:';
+		req.headers['x-whistle-https'] = 'true';
+	}
 	options.host = '127.0.0.1';
 	options.method = req.method;
 	options.hostname = null;
@@ -24,6 +28,15 @@ function request(req, res, port, weinre) {
 	}));
 }
 
+function response(req, res, statusCode, msg) {
+	res.writeHead(statusCode || 404, {
+    	'content-type': 'text/html; charset=utf-8'
+    });
+	res.src(util.wrapResponse({
+				body: msg || 'Not Found'
+			}));
+}
+
 module.exports = function(req, res, next) {
 	var host = req.headers.host || '';
 	var config = this.config;
@@ -38,10 +51,26 @@ module.exports = function(req, res, next) {
 		}
 	}
 	
+	var pluginMgr = this.pluginMgr;
+	var homePage;
 	if (host == config.localUIHost) {
 		request(req, res, config.uiport);
 	} else if (host == config.WEINRE_HOST) {
 		request(req, res, config.weinreport, true);
+	} else if (homePage = pluginMgr.getPluginFromHomePage(util.getFullUrl(req))) {
+		pluginMgr.loadPlugin(homePage, function(err, ports) {
+			if (err) {
+				response(req, res, 503, err);
+				return;
+			}
+			
+			if (!ports.uiPort) {
+				response(req, res);
+				return;
+			}
+			
+			request(req, res, ports.uiPort);
+		});
 	} else {
 		next();
 	}
