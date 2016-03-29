f4f02dad123852d8d0897eea206e211f83573a7f
avwo <avwu@qq.com>
2016-03-29 20:22:34 +0800

    feature: disable all rules

--- biz/webui/cgi-bin/rules/disable-all-rules.js
+++ biz/webui/cgi-bin/rules/disable-all-rules.js
@@ -2,7 +2,7 @@ var properties = require('../../lib/properties');
 var rules = require('../../lib/rules');
 
 module.exports = function(req, res) {
-	properties.set('disabledAllRules', req.body.disableAllRules == 1);
+	properties.set('disabledAllRules', req.body.disabledAllRules == 1);
 	rules.parseRules();
 	res.json({ec: 0, em: 'success'});
 };
