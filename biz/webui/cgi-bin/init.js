d287522dfdf800ea3ac7860f983d013997426636
avwo <avwu@qq.com>
2016-04-14 23:50:38 +0800

    feat: show plugin rules

--- biz/webui/cgi-bin/init.js
+++ biz/webui/cgi-bin/init.js
@@ -17,6 +17,7 @@ module.exports = function(req, res) {
 		interceptHttpsConnects: properties.get('interceptHttpsConnects'),
 		filterText: properties.get('filterText'),
 		plugins: pluginMgr.getPlugins(),
+		pluginsRules: pluginMgr.getRules(),
 		disabledAllRules: properties.get('disabledAllRules')
 	});
 };
---
+++ biz/webui/cgi-bin/plugins/disable-rules.js
@@ -0,0 +1,7 @@
+var properties = require('../../lib/properties');
+var rules = require('../../lib/rules');
+
+module.exports = function(req, res) {
+	
+	res.json({ec: 0, em: 'success'});
+};
