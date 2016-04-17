bb60903680ead1d4bd3a0f3eaa22dd8a5f55989d
avwo <avwu@qq.com>
2016-04-17 13:50:07 +0800

    feat: Output version

--- biz/webui/cgi-bin/plugins/get-plugins-and-rules.js
+++ biz/webui/cgi-bin/plugins/get-plugins-and-rules.js
@@ -1,9 +1,12 @@
 var properties = require('../../lib/properties');
+var config = require('../../lib/config');
 var pluginMgr = require('../../lib/proxy').pluginMgr;
 
 module.exports = function(req, res) {
 	
 	res.json({
+		version: config.version,
+		latestVersion: properties.get('latestVersion'),
 		plugins: pluginMgr.getPlugins(),
 		pluginsRules: pluginMgr.getRules(),
 		disabledPlugins: properties.get('disabledPlugins') || {},
