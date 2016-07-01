06fccee365aa9d08447a5eb68a7a609fe41f57db
avwo <avwu@qq.com>
2016-07-01 11:16:04 +0800

    feat: Use req.ip instead of mac

--- biz/webui/cgi-bin/util.js
+++ biz/webui/cgi-bin/util.js
@@ -3,17 +3,27 @@ var config = require('../lib/config');
 var properties = require('../lib/properties');
 
 exports.getServerInfo = function getServerInfo(req) {
+	var info = {
+			version: config.version,
+			nodeVersion: process.version,
+			latestVersion: properties.get('latestVersion'),
+			host: os.hostname(),
+			port: config.port,
+			weinrePort: config.weinreport,
+			ipv4: [],
+			ipv6: [],
+			mac: req.ip
+		};
+	var ifaces = os.networkInterfaces();
+	Object.keys(ifaces).forEach(function(ifname) {
+		 ifaces[ifname].forEach(function (iface) {
+			    if (iface.internal) {
+			      return;
+			    }
+			    info[iface.family == 'IPv4' ? 'ipv4' : 'ipv6'].push(iface.address);
+			  });
+	});
 	
-	return {
-		version: config.version,
-		nodeVersion: process.version,
-		latestVersion: properties.get('latestVersion'),
-		host: os.hostname(),
-		port: config.port,
-		weinrePort: config.weinreport,
-		ipv4: [],
-		ipv6: [],
-		mac: req.ip
-	};
+	return info;
 };
 
