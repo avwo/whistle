67dc87c173cdd91a154084ce303404d4c9d52338
avwo <avwu@qq.com>
2016-08-03 14:33:10 +0800

    test: refine code

--- test/index.test.js
+++ test/index.test.js
@@ -1,24 +1,15 @@
 var startWhistle = require('../index');
 var util = require('./util.test');
 var config = require('./config.test');
+var testList = ['plugin', 'host', 'rule', 'reqSpeed'].map(function(name) {
+	return require('./' + name + '.test');
+});
 
 startWhistle({
 	port: config.port
 }, function() {
-	var startTime = Date.now();
-	util.request({
-		url: 'http://test.whistlejs.com/',
-		body: 'test'
-	}, function(res, data) {
-		if (data) {
-			console.log(data.body);
-		}
-	});
-	
-	util.request('http://reqdelay.test.whistlejs.com', function(res, data) {
-		if (data) {
-			console.log(Date.now() - startTime > 1000);
-		}
+	testList.forEach(function(fn) {
+		fn();
 	});
 });
 
--- test/rule.test.js
+++ test/rule.test.js
@@ -1,5 +1,12 @@
 var util = require('./util.test');
 
 module.exports = function() {
-	
+	util.request({
+		url: 'http://test.whistlejs.com/',
+		body: 'test'
+	}, function(res, data) {
+		if (data) {
+			console.log(data.body);
+		}
+	});
 };
