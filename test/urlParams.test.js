2792451d7226d248cad4857da464e2d303eda2fc
avwo <avwu@qq.com>
2016-08-04 23:07:21 +0800

    test: urlParams

--- test/urlParams.test.js
+++ test/urlParams.test.js
@@ -2,6 +2,6 @@ var util = require('./util.test');
 
 module.exports = function() {
 	util.request('http://urlparams.test.whistlejs.com/index.html', function(res, data) {
-		/\?test=abc/.test(data.url).should.be.true();
+		data.url.substring(data.url.indexOf('?') + 1).should.equal('test=abc');
 	});
 };
