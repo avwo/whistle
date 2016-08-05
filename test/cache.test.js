68ad225af58ec20b086a42d17b4af8e29827f61d
avwo <avwu@qq.com>
2016-08-05 22:42:24 +0800

    test: cache

--- test/cache.test.js
+++ test/cache.test.js
@@ -4,4 +4,11 @@ module.exports = function() {
 	util.request('http://cache.test.whistlejs.com/index.html', function(res, data) {
 		res.headers.should.have.property('cache-control', 'max-age=60000');
 	});
+	
+	util.request({
+		method: 'post',
+		url: 'https://cache.test.whistlejs.com/index.html'
+	}, function(res, data) {
+		res.headers.should.have.property('cache-control', 'max-age=60000');
+	});
 };
