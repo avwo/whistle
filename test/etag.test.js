b6729db2d5bea646609fc363efad2586113a7223
avwo <avwu@qq.com>
2016-08-05 23:38:06 +0800

    test: etag

--- test/etag.test.js
+++ test/etag.test.js
@@ -4,4 +4,12 @@ module.exports = function() {
 	util.request('http://etag.test.whistlejs.com/index.html', function(res, data) {
 		data.headers.should.have.property('etag', 'xxx');
 	});
+	
+	util.request({
+		url: 'http://etag.test.whistlejs.com/index.html',
+		method: 'post',
+		body: 'sssssss'
+	}, function(res, data) {
+		data.headers.should.have.property('etag', 'xxx');
+	});
 };
