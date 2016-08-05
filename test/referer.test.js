38887848066a3fd20dd5096764e1b76791560d12
avwo <avwu@qq.com>
2016-08-05 22:38:32 +0800

    test: referer

--- test/referer.test.js
+++ test/referer.test.js
@@ -4,4 +4,11 @@ module.exports = function() {
 	util.request('http://referer.test.whistlejs.com/index.html', function(res, data) {
 		data.headers.should.have.property('referer', 'xxx');
 	});
+	
+	util.request({
+		url: 'https://referer.test.whistlejs.com/index.html',
+		method: 'post'
+	}, function(res, data) {
+		data.headers.should.have.property('referer', 'xxx');
+	});
 };
