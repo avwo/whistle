d68b82def045a3cf6a259cd41383eb24add5f2dc
avwo <avwu@qq.com>
2016-08-05 23:39:54 +0800

    test: res.prepend.body.append

--- test/index.test.js
+++ test/index.test.js
@@ -9,7 +9,7 @@ var testList = ['host', 'rule', 'reqSpeed', 'resSpeed', 'reqDelay', 'resDelay', 
                 'referer', 'urlParams', 'params', 'ua', 'reqCharset', 'resCharset', 'reqType', 
                 'resType', 'reqCookies', 'resCookies', 'reqPrepend', 'resPrepend', 'accept', 'etag',
                 'cache', 'statusCode', 'redirect', 'replaceStatus', 'reqBody', 'resBody', 'reqAppend', 'resAppend',
-                'reqReplace', 'resReplace', 'req.prepend.body.append'].map(function(name) {
+                'reqReplace', 'resReplace', 'req.prepend.body.append', 'res.prepend.body.append'].map(function(name) {
 	return require('./' + name + '.test');
 });
 var count = 2;
---
+++ test/res.prepend.body.append.test.js
@@ -0,0 +1,14 @@
+var util = require('./util.test');
+
+module.exports = function() {
+	util.request({
+		url: 'https://resprepend.resbody.resappend.test.whistlejs.com/',
+		method: 'post'
+	}, function(res, data) {
+		data.body.should.equal('prependbodyappend');
+	});
+	
+	util.request('http://resprepend.resbody.resappend.test.whistlejs.com/', function(res, data) {
+		data.body.should.equal('prependbodyappend');
+	});
+};
