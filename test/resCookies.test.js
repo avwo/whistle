3208a10c009dc2723635f31726d83288729380ea
avwo <avwu@qq.com>
2016-08-05 13:46:45 +0800

    test: resCookies

--- test/resCookies.test.js
+++ test/resCookies.test.js
@@ -2,6 +2,6 @@ var util = require('./util.test');
 
 module.exports = function() {
 	util.request('http://rescookies.test.whistlejs.com/', function(res, data) {
-		
+		res.headers['set-cookie'].should.containEql('key2=value2');
 	});
 };
