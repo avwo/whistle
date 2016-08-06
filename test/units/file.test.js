dfda68c3bfa6411db48be1da1746d6ac7186baa5
avwo <avwu@qq.com>
2016-08-07 00:55:46 +0800

    test: file

--- test/units/file.test.js
+++ test/units/file.test.js
@@ -1,7 +1,6 @@
 var util = require('../util.test');
 
 module.exports = function() {
-	var now = Date.now();
 	util.request('http://file.test.whistlejs.com/index.html', function(res, data) {
 		data.should.have.property('body', 'html');
 	});
