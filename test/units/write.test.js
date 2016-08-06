0020dd810808993ce87fa8c5fa7a2199b2f029a0
avwo <avwu@qq.com>
2016-08-06 15:00:38 +0800

    test: write

--- test/units/write.test.js
+++ test/units/write.test.js
@@ -2,27 +2,17 @@ var util = require('../util.test');
 
 module.exports = function() {
 	var now = Date.now();
-	util.request('http://reqwrite.test.whistlejs.com/index.html', function(res, data) {
-		
-	});
-	
+	util.request('http://reqwrite.test.whistlejs.com/index.html');
 	util.request({
 		url: 'https://reqwrite.test.whistlejs.com/index.html',
 		method: 'post',
 		body: util.getTextBySize(32)
-	}, function(res, data) {
-		
 	});
 	
-//	util.request('https://reswriteraw.test.whistlejs.com/index.html', function(res, data, err) {
-//		console.log(err);
-//	});
-//	
-//	util.request({
-//		url: 'http://reswriteraw.test.whistlejs.com/index.html',
-//		method: 'post',
-//		body: util.getTextBySize(128)
-//	}, function(res, data) {
-//		console.log(data);
-//	});
+	util.request('https://reswrite2.test.whistlejs.com/index.html');
+	util.request({
+		url: 'http://reswrite2.test.whistlejs.com/index.html',
+		method: 'post',
+		body: util.getTextBySize(128)
+	});
 };
