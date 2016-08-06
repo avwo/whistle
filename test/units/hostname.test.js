8dcbb9161c138b80b28a5f5309919c48eada87c4
avwo <avwu@qq.com>
2016-08-06 15:30:15 +0800

    test: hostname

---
+++ test/units/hostname.test.js
@@ -0,0 +1,15 @@
+var util = require('../util.test');
+
+module.exports = function() {
+	util.request('http://hostname.test.whistlejs.com/index.html', function(res, data) {
+		data.headers.should.have.property('hostname', 'xxx');
+	});
+	
+	util.request({
+		url: 'http://hostname.test.whistlejs.com/index.html',
+		method: 'post',
+		body: 'sssssss'
+	}, function(res, data) {
+		data.headers.should.have.property('hostname', 'xxx');
+	});
+};
