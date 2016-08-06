7175637eebdae4e27d0c289b7fb95bd33221a98e
avwo <avwu@qq.com>
2016-08-06 22:36:02 +0800

    test: ui

--- test/units/ui.test.js
+++ test/units/ui.test.js
@@ -10,9 +10,149 @@ module.exports = function() {
 	util.request('http://local.whistlejs.com/cgi-bin/values/list');
 	util.request('http://local.whistlejs.com/cgi-bin/plugins/get-plugins');
 	util.request('http://local.whistlejs.com/cgi-bin/rules/list');
-	util.request('http://local.whistlejs.com/cgi-bin/values/list');
-	util.request('http://local.whistlejs.com/cgi-bin/values/list');
-	util.request('http://local.whistlejs.com/cgi-bin/values/list');
-	util.request('http://local.whistlejs.com/cgi-bin/values/list');
-	util.request('http://local.whistlejs.com/cgi-bin/values/list');
+	
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/add',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/remove',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/rename',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/set-current',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/set-font-size',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/set-theme',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/show-line-numbers',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/move-up',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/values/move-down',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/plugins/disable-plugin',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/plugins/disable-all-plugins',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/disable-all-rules',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/add',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/disable-default',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/enable-default',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/remove',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/rename',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/select',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/set-current',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/set-font-size',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/set-theme',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/show-line-numbers',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/unselect',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/allow-multiple-choice',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/sync-with-sys-hosts',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/set-sys-hosts',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/log/set',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/composer',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/set-filter',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/intercept-https-connects',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/hide-https-connects',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/do-not-show-again',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/check-update',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/move-down',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/move-up',
+		method: 'post'
+	});
+	util.request({
+		url: 'http://local.whistlejs.com/cgi-bin/rules/get-sys-hosts',
+		method: 'post'
+	});
 };
