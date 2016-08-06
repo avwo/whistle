e302dda3107aad81d0079f088f8374d8e94d90f2
avwo <avwu@qq.com>
2016-08-06 10:17:42 +0800

    test: js

--- test/js.test.js
+++ test/js.test.js
@@ -2,13 +2,20 @@ var util = require('./util.test');
 
 module.exports = function() {
 	util.request('http://js1.test.whistlejs.com/index.html?resBody=_', function(res, body) {
-		console.log(res.headers, body)
+		body.should.equal('_<script>js</script>');
 	});
 	
 	util.request({
 		method: 'post',
 		url: 'https://js2.test.whistlejs.com/index.html?resBody=_'
 	}, function(res, body) {
-		
+		body.should.equal('_js');
+	});
+	
+	util.request({
+		method: 'post',
+		url: 'https://js32.test.whistlejs.com/index.html?resBody=_'
+	}, function(res, body) {
+		body.should.equal('_');
 	});
 };
