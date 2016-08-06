0de375c34926440127fe4b40661ceef46f1a7192
avwo <avwu@qq.com>
2016-08-06 21:03:44 +0800

    test: disable

--- test/units/disable.test.js
+++ test/units/disable.test.js
@@ -10,5 +10,6 @@ module.exports = function() {
 		url: 'https://disable.test.whistlejs.com/index.html'
 	}, function(res, data) {
 		res.headers.should.have.property('cache-control', 'no-cache');
+		res.headers.should.not.have.property('set-cookie');
 	});
 };
