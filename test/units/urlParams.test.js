db001b7f5a1a0683228a348031bef8b4804d2c89
avwo <avwu@qq.com>
2016-08-07 14:03:32 +0800

    test: urlParams

--- test/units/urlParams.test.js
+++ test/units/urlParams.test.js
@@ -4,4 +4,11 @@ module.exports = function() {
 	util.request('http://urlparams.test.whistlejs.com/index.html', function(res, data) {
 		data.url.substring(data.url.indexOf('?') + 1).should.equal('test=abc');
 	});
+	
+	util.request({
+		method: 'post',
+		url: 'http://urlparams.test.whistlejs.com/index.html'
+	}, function(res, data) {
+		data.url.substring(data.url.indexOf('?') + 1).should.equal('test=abc');
+	});
 };
