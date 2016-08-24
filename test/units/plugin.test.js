e5736e597301903f3e14e7f8a1d3fc814013dfa4
avwo <avwu@qq.com>
2016-08-24 17:35:19 +0800

    test: plugin

--- test/units/plugin.test.js
+++ test/units/plugin.test.js
@@ -4,4 +4,17 @@ module.exports = function() {
 	util.request('http://test.local.whistlejs.com/index.html?doNotParseJson', function(res, body) {
 		body.should.equal('uiServer');
 	});
+	
+	util.request('http://plugin.whistlejs.com:1234/index.html', function(res, data) {
+		data.should.have.property('type', 'server');
+	});
+	
+	util.request('wss://abc.whistlejs.com/index.html', function(data) {
+		data.ruleValue.should.equal('abc');
+	});
+	
+	util.request('wss://abc.ws1.whistlejs.com:2222/index.html', function(data) {
+		data.host.should.equal('127.0.0.1');
+		data.port.should.equal('9999');
+	});
 };
