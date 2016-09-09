fdba95e8691bcdbf7033a67bdca5354d97121136
avwo <avwu@qq.com>
2016-09-10 02:24:38 +0800

    test: tunnel

--- test/units/tunnel.test.js
+++ test/units/tunnel.test.js
@@ -3,4 +3,7 @@ var config = require('../config.test');
 
 module.exports = function() {
 	util.proxy('http://test.whistlejs.com');
+	util.proxy('http://tnl1.whistlejs.com');
+	util.proxy('http://tnl2.whistlejs.com');
+	util.proxy('http://tnl3.whistlejs.com');
 };
