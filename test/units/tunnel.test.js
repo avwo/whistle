cad3ecf528f847e751590268fd5136a85ea6534d
avwo <avwu@qq.com>
2016-09-10 02:01:37 +0800

    test: x-whistle-policy

--- test/units/tunnel.test.js
+++ test/units/tunnel.test.js
@@ -2,7 +2,5 @@ var util = require('../util.test');
 var config = require('../config.test');
 
 module.exports = function() {
-	util.proxy('http://127.0.0.1:' + config.serverPort, function(res) {
-		
-	});
+	util.proxy('http://test.whistlejs.com');
 };
