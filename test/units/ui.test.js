31be14cb8d855fbc45343cac0c2198a93cafc883
avwo <avwu@qq.com>
2016-09-16 21:33:11 +0800

    test: ui port

--- test/units/ui.test.js
+++ test/units/ui.test.js
@@ -3,8 +3,10 @@ var util = require('../util.test');
 
 module.exports = function() {
 	util.request('http://local.whistlejs.com/index.html?doNotParseJson');
+	util.request('http://local.whistlejs.com:1234/index.html?doNotParseJson');
 	util.request('http://local.whistlejs.com/cgi-bin/log/get');
 	util.request('http://local.whistlejs.com/cgi-bin/init');
+	util.request('http://local.whistlejs.com:2345/cgi-bin/init');
 	util.request('http://local.whistlejs.com/cgi-bin/get-data');
 	util.request('http://local.whistlejs.com/cgi-bin/server-info');
 	util.request('http://local.whistlejs.com/cgi-bin/values/list');
