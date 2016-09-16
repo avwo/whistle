3b43f37090a63ff1f49785cee7e6bf586c3b5b4b
avwo <avwu@qq.com>
2016-09-16 21:32:21 +0800

    test: ui port

--- test/units/weinre.test.js
+++ test/units/weinre.test.js
@@ -18,4 +18,22 @@ module.exports = function() {
 		method: 'post',
 		body: 'sssssss'
 	});
+	
+	util.request('http://weinre1.test.whistlejs.com:1234/index.html?resBody=_', function(res, body) {
+		body.should.containEql('<script src="http://weinre.local.whistlejs.com/target/target-script-min.js#xxx"></script>');
+	});
+	
+	util.request({
+		url: 'http://weinre2.test.whistlejs.com:2345/index.html',
+		method: 'post',
+		body: 'sssssss'
+	});
+	
+	util.request('https://weinre3.test.whistlejs.com:3456/index.html');
+	
+	util.request({
+		url: 'https://weinre4.test.whistlejs.com:4567/index.html',
+		method: 'post',
+		body: 'sssssss'
+	});
 };
