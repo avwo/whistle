57f638baca0b079924a2d189c9acc0530f60e6e5
avwo <avwu@qq.com>
2016-09-26 01:26:15 +0800

    test: uiServer

--- test/units/ws.test.js
+++ test/units/ws.test.js
@@ -4,6 +4,13 @@ module.exports = function() {
 	if (/^v0\.10\./.test(process.version)) {
 		return;
 	}
+	util.request('ws://test.local.whistlejs.com/cgi-bin/rules/list', function(data) {
+    data.headers.should.have.property('host', 'test.local.whistlejs.com');
+  });
+  util.request('wss://test.local.whistlejs.com/cgi-bin/rules/list', function(data) {
+    data.headers.should.have.property('host', 'test.local.whistlejs.com');
+  });
+  
 	util.request('ws://test.whistlejs.com/index.html', function(data) {
 		data.host.should.equal('127.0.0.1');
 		data.port.should.equal('8080');
