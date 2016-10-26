b102b8f64630409a9fc326e919555b88002f71f3
avwo <avwu@qq.com>
2016-10-27 04:01:54 +0800

    test: resReplace, reqReplace

--- test/units/ssi-include.test.js
+++ test/units/ssi-include.test.js
@@ -2,8 +2,8 @@ var util = require('../util.test');
 
 module.exports = function() {
   util.request('http://ssi-include.whistlejs.com/index.html?doNotParseJson', function(res, data) {
-    data.should.be.containEql('ssi1.html');
-    data.should.be.containEql('ssi2.html');
-    data.should.be.containEql('ssi3.html');
+    data.should.be.containEql('include1.html');
+    data.should.be.containEql('include2.html');
+    data.should.be.containEql('include3.html');
   });
 };
