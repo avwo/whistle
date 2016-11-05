6fe0c092c247086772e504760278c3d263985d1f
avwo <avwu@qq.com>
2016-11-05 22:03:03 +0800

    test: socks

--- test/units/socks.test.js
+++ test/units/socks.test.js
@@ -28,4 +28,32 @@ module.exports = function() {
   util.request('https://socks4.w2.org/index.html', function(res, data) {
     data.body.should.be.equal('test');
   });
+  
+  util.request('http://socks5.w2.org/index.html', function(res, data) {
+    data.port.should.be.equal(1080);
+  });
+  util.request('https://socks5.w2.org/index.html', function(res, data) {
+    data.body.should.be.equal('test');
+  });
+  
+  util.request('http://socks6.w2.org/index.html', function(res, data) {
+    data.port.should.be.equal(1118);
+  });
+  util.request('https://socks6.w2.org/index.html', function(res, data) {
+    data.body.should.be.equal('test');
+  });
+  
+  util.request('http://socks7.w2.org/index.html', function(res, data) {
+    data.port.should.be.equal(1080);
+  });
+  util.request('https://socks7.w2.org/index.html', function(res, data) {
+    data.body.should.be.equal('test');
+  });
+  
+  util.request('http://socks8.w2.org/index.html', function(res, data) {
+    data.port.should.be.equal(1118);
+  });
+  util.request('https://socks8.w2.org/index.html', function(res, data) {
+    data.body.should.be.equal('test');
+  });
 };
