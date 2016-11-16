4ff0e0b3e62d00796cf6afb1688ddd44f9e96ca7
avwo <avwu@qq.com>
2016-11-16 23:58:10 +0800

    test: rulesFile

---
+++ test/units/rulesFile.test.js
@@ -0,0 +1,7 @@
+var util = require('../util.test');
+
+module.exports = function() {
+//  util.request('http://rfs1.w2.org/index.html', function(res, data) {
+//    //data.headers.should.have.property('accept', 'xxx');
+//  });
+};
