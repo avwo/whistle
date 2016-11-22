522e484bddbb08a7b7632d77c7728d393c4f2fe0
avwo <avwu@qq.com>
2016-11-22 10:01:38 +0800

    test: plugins

--- test/units/plugins.test.js
+++ test/units/plugins.test.js
@@ -1,7 +1,7 @@
 var util = require('../util.test');
 
 module.exports = function() {
-  util.request('http://socks2.w2.org/index.html', function(res, body) {
-    res.headers['content-type'].should.be.equal('text/html;charset=utf8');
-  });
+//  util.request('http://socks2.w2.org/index.html', function(res, body) {
+//    res.headers['content-type'].should.be.equal('text/html;charset=utf8');
+//  });
 };
