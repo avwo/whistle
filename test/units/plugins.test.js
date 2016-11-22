2565bea586905fc363367825ebf823f7068fa5b5
avwo <avwu@qq.com>
2016-11-22 20:10:14 +0800

    test: plugins

--- test/units/plugins.test.js
+++ test/units/plugins.test.js
@@ -1,10 +1,8 @@
 var util = require('../util.test');
 
 module.exports = function() {
-  if (/^v0\.10\./.test(process.version)) { //node 0.10不支持socksv5这个模块，暂时屏蔽掉
-    return;
-  }
-  util.request('http://socks2.w2.org/index.html', function(res, body) {
+  util.request('http://mp1.w2.org/index.html', function(res, data) {
     res.headers['content-type'].should.be.equal('text/html; charset=gbk');
+    data.headers['x-whistle-rule-value'].should.be.equal('123');
   });
 };
