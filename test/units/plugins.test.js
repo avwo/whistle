8fe12598abf57a914736d1a98a5b65a7b44353d6
avwo <avwu@qq.com>
2016-11-22 15:22:28 +0800

    test: plugins

--- test/units/plugins.test.js
+++ test/units/plugins.test.js
@@ -1,6 +1,9 @@
 var util = require('../util.test');
 
 module.exports = function() {
+  if (/^v0\.10\./.test(process.version)) { //node 0.10不支持socksv5这个模块，暂时屏蔽掉
+    return;
+  }
   util.request('http://socks2.w2.org/index.html', function(res, body) {
     res.headers['content-type'].should.be.equal('text/html; charset=gbk');
   });
