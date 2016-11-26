646fd504be054c4ee7316db673e7b93769b78aba
avwo <avwu@qq.com>
2016-11-26 15:25:38 +0800

    test: delete://

---
+++ test/units/delete.test.js
@@ -0,0 +1,41 @@
+var util = require('../util.test');
+
+module.exports = function() {
+  util.request('http://delete.test.whistlejs.com/index.html', function(res, data) {
+    data.headers.should.not.have.property('x-delete-all');
+    data.headers.should.not.have.property('x-delete-req');
+    res.headers.should.not.have.property('x-delete-res');
+  });
+
+  util.request({
+    method: 'post',
+    headers: {
+      'x-delete-test': 123
+    },
+    url: 'https://delete.test.whistlejs.com/index.html'
+  }, function(res, data) {
+    data.headers.should.have.property('x-delete-test');
+    data.headers.should.not.have.property('x-delete-all');
+    data.headers.should.not.have.property('x-delete-req');
+    res.headers.should.not.have.property('x-delete-res');
+  });
+  
+  util.request('http://delete1.test.whistlejs.com/index.html', function(res, data) {
+    data.headers.should.have.property('x-delete-all');
+    data.headers.should.have.property('x-delete-req');
+    res.headers.should.have.property('x-delete-res');
+  });
+
+  util.request({
+    method: 'post',
+    headers: {
+      'x-delete-test': 123
+    },
+    url: 'https://delete1.test.whistlejs.com/index.html'
+  }, function(res, data) {
+    data.headers.should.have.property('x-delete-test');
+    data.headers.should.have.property('x-delete-all');
+    data.headers.should.have.property('x-delete-req');
+    res.headers.should.have.property('x-delete-res');
+  });
+};
