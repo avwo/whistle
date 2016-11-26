6788e92877be6b4df6fdf77ab8deefaae662a7e4
avwo <avwu@qq.com>
2016-11-26 15:29:27 +0800

    test: websocket

--- test/units/ws.test.js
+++ test/units/ws.test.js
@@ -12,12 +12,10 @@ module.exports = function() {
   });
 
   util.request('ws://test.whistlejs.com/index.html', function(data) {
-    data.host.should.equal('127.0.0.1');
-    data.port.should.equal('8080');
+    data.host.should.equal('127.0.0.1:8080');
   });
 
   util.request('wss://ws1.test.whistlejs.com/index.html', function(data) {
-    data.host.should.equal('127.0.0.1');
-    data.port.should.equal('9999');
+    data.host.should.equal('127.0.0.1:8080');
   });
 };
