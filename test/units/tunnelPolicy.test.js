98b89f551fc0f1373f177619c77f1dc78c83b820
avwo <avwu@qq.com>
2016-11-27 01:34:57 +0800

    test: tunnel.headers[x-whistle-policy] = tunnel

--- test/units/tunnelPolicy.test.js
+++ test/units/tunnelPolicy.test.js
@@ -1,10 +1,10 @@
 var util = require('../util.test');
 
 module.exports = function() {
-  util.request('https://tp.w2.org/index.html', function(res, data) {
-    data.ruleValue.should.be.equal('global');
-  });
-
+//  util.request('https://tp.w2.org/index.html', function(res, data) {
+//    data.ruleValue.should.be.equal('global');
+//  });
+//
 //  util.request({
 //    method: 'post',
 //    url: 'https://tp.w2.org/index.html',
