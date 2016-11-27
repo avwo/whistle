36fdf67cc1ba66bd408a5d7e67422e6d99ae34f8
avwo <avwu@qq.com>
2016-11-27 20:26:46 +0800

    test: tunnel proxy

--- test/units/plugins.test.js
+++ test/units/plugins.test.js
@@ -5,4 +5,8 @@ module.exports = function() {
     res.headers['content-type'].should.be.equal('text/html; charset=gbk');
     data.headers['x-whistle-rule-value'].should.be.equal('123');
   });
+  
+  util.request('https://ths.w2.org/index.html', function(res, data) {
+    data.test.should.be.equal('values');
+  });
 };
