c689fc0047efb95417a6b1dfc3d40c06df227baf
avwo <avwu@qq.com>
2016-11-27 09:52:51 +0800

    test: Add whistle.test3

--- test/units/tunnelPolicy.test.js
+++ test/units/tunnelPolicy.test.js
@@ -1,17 +1,15 @@
 var util = require('../util.test');
 
 module.exports = function() {
-  util.request('https://tp.w2.org/index.html?doNotParseJson', function(res, data, err) {
-//    data.ruleValue.should.be.equal('global');
-    console.log('get', data, err&&err.stack);
+  util.request('https://tp.w2.org/index.html', function(res, data, err) {
+    data.ruleValue.should.be.equal('global');
   });
 
   util.request({
     method: 'post',
-    url: 'https://tp.w2.org/index.html?doNotParseJson',
+    url: 'https://tp.w2.org/index.html',
     isTunnel: true
   }, function(res, data, err) {
-//    data.body.should.be.equal('test');
-    console.log('post', data, err&&err.stack);
+    data.body.should.be.equal('test');
   });
 };
