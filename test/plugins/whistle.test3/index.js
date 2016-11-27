849af13352769e69a16b2733858d890562c3f2ac
avwo <avwu@qq.com>
2016-11-27 19:52:20 +0800

    test: rulesFile://

--- test/plugins/whistle.test3/index.js
+++ test/plugins/whistle.test3/index.js
@@ -1,5 +1,5 @@
 exports.reqRulesServer = function(server) {
   server.on('request', function(req, res) {
-    res.end('tp.w2.org/index.html resType://html');
+    res.end('tp.w2.org/index.html resType://html rulesFile://{rulesFile.js}');
   });
 };
