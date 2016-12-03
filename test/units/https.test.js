c3308a6da1a6c20d254ad661d669e793f7766c43
avwo <avwu@qq.com>
2016-12-03 14:38:07 +0800

    test: proxy agent

--- test/units/https.test.js
+++ test/units/https.test.js
@@ -22,6 +22,10 @@ module.exports = function() {
     data.type.should.be.equal('server');
   });
   
+  util.request('http://https3.w2.org:5566/index.html', function(res, data) {
+    data.body.should.be.equal('test');
+  });
+  
   util.request('http://whistle-ssl.https.w2.org:' + config.httpsPort + '/index.html', function(res, data) {
     data.body.should.be.equal('test');
   });
