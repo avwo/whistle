d7e938d297fe63ef470b317a06df3e8ffde3cc63
avwo <avwu@qq.com>
2016-12-03 13:55:12 +0800

    test: filter://https

--- test/plugins/whistle.test1/lib/rulesServer.js
+++ test/plugins/whistle.test1/lib/rulesServer.js
@@ -1,6 +1,6 @@
 
 module.exports = function(server, options) {
   server.on('request', function(req, res) {
-    res.end('mp1.w2.org/index.html resType://html');
+    res.end('mp1.w2.org/index.html resType://html\nhttps2.w2.org filter://https');
   });
 };
