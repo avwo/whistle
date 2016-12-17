b90a383941bc5f33d66a93b6ba7a2b37a6e8a491
avwo <avwu@qq.com>
2016-12-17 14:34:58 +0800

    test: server-agent

--- test/units/server-agent.test.js
+++ test/units/server-agent.test.js
@@ -2,8 +2,6 @@ var util = require('../util.test');
 
 module.exports = function() {
   for (var i = 0; i < 600; i++) {
-    util.request('https://' + i + '.server-agent.com/', function(res, data) {
-      console.log(data)
-    });
+    util.request('https://' + i + '.server-agent.com/');
   }
 };
