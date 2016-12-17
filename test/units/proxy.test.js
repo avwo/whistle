be07c111511fc900414350b7d2627c3423fca840
avwo <avwu@qq.com>
2016-12-17 13:42:35 +0800

    test: proxy websocket

--- test/units/proxy.test.js
+++ test/units/proxy.test.js
@@ -13,4 +13,7 @@ module.exports = function() {
     data.should.have.property('url', 'http://proxy.test.whistlejs.com/index.html');
   });
 
+  util.request('ws://ws2.w2.org:8081/index.html', function(res, data) {
+    res.type.should.be.equal('server');
+  });
 };
