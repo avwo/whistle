0d608c401971003d278ec409a8b6c05c278d7340
avwo <avwu@qq.com>
2016-12-17 12:57:51 +0800

    test: accept-encoding

--- test/units/accept.test.js
+++ test/units/accept.test.js
@@ -1,7 +1,12 @@
 var util = require('../util.test');
 
 module.exports = function() {
-  util.request('http://accept.test.whistlejs.com/index.html', function(res, data) {
+  util.request({
+    url: 'http://accept.test.whistlejs.com/index.html',
+    headers: {
+      'accept-encoding': 'deflate, gzip'
+    }
+  }, function(res, data) {
     data.headers.should.have.property('accept', 'xxx');
   });
 };
