15761ba247da895032be82211f3b7b4cebd02d06
avwo <avwu@qq.com>
2016-12-20 09:06:35 +0800

    test: statsServer

--- test/plugins/whistle.test2/index.js
+++ test/plugins/whistle.test2/index.js
@@ -1,6 +1,5 @@
 exports.statsServer = function(server, options) {
   server.on('request', function(req, res) {
-    console.log(decodeURIComponent(req.headers[options.FULL_URL_HEADER]))
     res.end();
   });
 };
