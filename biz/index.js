60df08353dce9a101e2a54c0d13e8183f65ee350
avwo <avwu@qq.com>
2017-04-18 10:21:54 +0800

    refactor: Refine code

--- biz/index.js
+++ biz/index.js
@@ -1,36 +1,5 @@
-var url = require('url');
 var net = require('net');
-var http = require('http');
 var util = require('../lib/util');
-
-function request(req, res, port, weinre) {
-  var options = url.parse(util.getFullUrl(req));
-  if (options.protocol == 'https:') {
-    options.protocol = 'http:';
-    req.headers['x-whistle-https'] = 'true';
-  }
-  options.host = '127.0.0.1';
-  options.method = req.method;
-  options.hostname = null;
-  options.protocol = null;
-  if (port) {
-    options.port = port;
-  }
-  req.headers['x-forwarded-for'] = req.clientIp;
-  options.headers = req.headers;
-  req.pipe(http.request(options, function(_res) {
-    if (weinre && options.pathname == '/target/target-script-min.js') {
-      _res.headers['access-control-allow-origin'] = '*';
-    }
-    if (util.getStatusCode(_res.statusCode)) {
-      res.writeHead(_res.statusCode, _res.headers);
-      res.src(_res);
-      _res.trailers && res.addTrailers(_res.trailers);
-    } else {
-      util.sendStatusCodeError(res, _res);
-    }
-  }));
-}
 
 module.exports = function(req, res, next) {
   var config = this.config;
@@ -53,9 +22,9 @@ module.exports = function(req, res, next) {
     host = '';
   }
   if (req.headers[config.WEBUI_HEAD] || config.isLocalUIUrl(host)) {
-    request(req, res, config.uiport);
+    util.transformReq(req, res, config.uiport);
   } else if (config.isWeinreUrl(host)) {
-    request(req, res, config.weinreport, true);
+    util.transformReq(req, res, config.weinreport, true);
   } else if (pluginHomePage || (pluginHomePage = pluginMgr.getPluginByHomePage(fullUrl))) {
     pluginMgr.loadPlugin(pluginHomePage, function(err, ports) {
       if (err || !ports.uiPort) {
@@ -68,7 +37,7 @@ module.exports = function(req, res, next) {
         }));
         return;
       }
-      request(req, res, ports.uiPort);
+      util.transformReq(req, res, ports.uiPort);
     });
   } else {
     next();
