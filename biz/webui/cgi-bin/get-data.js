f509c194a040b8135fc04bca8e691bd6ba33c7b5
avwo <avwu@qq.com>
2017-07-07 13:33:52 +0800

    refactor: Plugins

--- biz/webui/cgi-bin/get-data.js
+++ biz/webui/cgi-bin/get-data.js
@@ -1,6 +1,8 @@
 var proxy = require('../lib/proxy');
 var util = require('./util');
 var getData = require('../lib/data');
+var properties = require('../lib/properties');
+var pluginMgr = require('../lib/proxy').pluginMgr;
 
 var logger = proxy.logger;
 
@@ -16,6 +18,9 @@ module.exports = function(req, res) {
     server: util.getServerInfo(req),
     log: proxy.getLogs(data.startLogTime, data.count),
     svrLog: logger.getLogs(data.startSvrLogTime, data.count),
+    plugins: pluginMgr.getPlugins(),
+    disabledPlugins: properties.get('disabledPlugins') || {},
+    disabledAllPlugins: properties.get('disabledAllPlugins'),
     data: getData(data)
   });
 };
