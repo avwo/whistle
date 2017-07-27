1b714ac7c45a68420be6b447339a4b172a8907d5
avwo <avwu@qq.com>
2017-07-28 01:21:16 +0800

    feat: Refine filter

--- biz/webui/htdocs/js/inject.js
+++ biz/webui/htdocs/js/inject.js
@@ -1,4 +1,4 @@
-!function(e){function t(r){if(n[r])return n[r].exports;var o=n[r]={exports:{},id:r,loaded:!1};return e[r].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}({0:function(e,t,n){n(5)},5:function(e,t,n){var r,o;/*!
+!function(e){function t(r){if(n[r])return n[r].exports;var o=n[r]={exports:{},id:r,loaded:!1};return e[r].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}({0:function(e,t,n){n(6)},6:function(e,t,n){var r,o;/*!
 	 * jQuery JavaScript Library v2.1.4
 	 * http://jquery.com/
 	 *
