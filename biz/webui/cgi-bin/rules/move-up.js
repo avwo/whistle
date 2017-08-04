e289fdc66c758bfc4d481981ac0a7d853689eab0
avwo <avwu@qq.com>
2017-08-04 20:15:20 +0800

    refactor: code

--- biz/webui/cgi-bin/rules/move-up.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = function(req, res) {
-  res.json({ec: 0, em: 'success'});
-};
--- biz/webui/cgi-bin/server-info.js
+++
@@ -1,5 +0,0 @@
-var util = require('./util');
-
-module.exports = function(req, res) {
-  res.json({ec: 0, server: util.getServerInfo(req)});
-};
