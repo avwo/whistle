77116a568724c19817e729a0bd8427cfb231ea2e
avwo <avwu@qq.com>
2017-08-04 13:27:42 +0800

    feat: Support list drag and drop sort

---
+++ biz/webui/cgi-bin/rules/move-to.js
@@ -0,0 +1,5 @@
+// var rules = require('../../lib/rules');
+
+module.exports = function(req, res) {
+  res.json({ec: 0, em: 'success'});
+};
---
+++ biz/webui/cgi-bin/values/move-to.js
@@ -0,0 +1,5 @@
+// var values = require('../../lib/values');
+
+module.exports = function(req, res) {
+  res.json({ec: 0, em: 'success'});
+};
