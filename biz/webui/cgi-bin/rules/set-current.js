59f7f15c43d136c65618e6c703a0cdb17f26c9d3
avwo <avwu@qq.com>
2017-08-24 00:49:35 +0800

    refactor: Store active status in localStorage

--- biz/webui/cgi-bin/rules/set-current.js
+++
@@ -1,7 +0,0 @@
-var properties = require('../../lib/properties');
-
-module.exports = function(req, res) {
-  properties.set('currentRulesFile', req.body.name);
-  res.json({ec: 0, em: 'success'});
-};
-
--- biz/webui/cgi-bin/values/set-current.js
+++
@@ -1,7 +0,0 @@
-var properties = require('../../lib/properties');
-
-module.exports = function(req, res) {
-  properties.set('currentValuesFile', req.body.name);
-  res.json({ec: 0, em: 'success'});
-};
-
