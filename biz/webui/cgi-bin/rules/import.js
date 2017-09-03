d3919ea212d0ab0971ae9e89d58b012bb6b5b1a1
avwo <avwu@qq.com>
2017-09-04 00:11:44 +0800

    feat: Import Rules&Values

--- biz/webui/cgi-bin/rules/import.js
+++ biz/webui/cgi-bin/rules/import.js
@@ -1,5 +1,14 @@
 var get = require('./index');
+var getReqData = require('../util').getReqData;
+var rulesUtil = require('../../lib/rules-util');
 
 module.exports = function(req, res) {
-  res.json(get());
+  getReqData(req, function(err, result) {
+    if (err) {
+      res.status(200).json({ ec: 2, em: err.message });
+    } else {
+      rulesUtil.addRules(result.data, result.replace, req.query.clientId);
+      res.json(get());
+    }
+  });
 };
--- biz/webui/cgi-bin/util.js
+++ biz/webui/cgi-bin/util.js
@@ -2,6 +2,7 @@ var util = require('../lib/util');
 var config = require('../lib/config');
 var properties = require('../lib/properties');
 
+var MAX_OBJECT_SIZE = 1024  * 1;
 var index = 0;
 
 exports.getClientId = function() {
@@ -38,4 +39,36 @@ exports.getServerInfo = function getServerInfo(req) {
   return info;
 };
 
-
+var DATA_RE = /[\r\n]\s*(\{[\s\S]*\})[\r\n]/;
+var REPLACE_RE = /[\r\n]1[\r\n]/;
+exports.getReqData = function(req, callback) {
+  var result = '';
+  req.on('data', function(chunk) {
+    result = result ? Buffer.concat([result, chunk]) : chunk;
+    if (result.length > MAX_OBJECT_SIZE) {
+      req.removeAllListeners('data');
+      callback(new Error('The file size can not exceed 6m.'));
+    }
+  });
+  req.on('error', callback);
+  req.on('end', function() {
+    result += '';
+    var data;
+    result = result.replace(DATA_RE, function(all, match) {
+      data = match;
+      return '';
+    });
+    if (!data) {
+      return callback(new Error('The file content is not a JSON object'));
+    }
+    try {
+      data = JSON.parse(data);
+    } catch(err) {
+      return callback(err);
+    }
+    callback(null, {
+      data: data,
+      replace: REPLACE_RE.test(result)
+    });
+  });
+};
