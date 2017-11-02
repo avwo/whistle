a37bbdcbee17a6d2733bbc8b067c909860913d26
avwo <avwu@qq.com>
2017-11-02 13:30:16 +0800

    test: reqSpeed

--- test/units/reqSpeed.test.js
+++ test/units/reqSpeed.test.js
@@ -4,6 +4,7 @@ module.exports = function() {
   var now = Date.now();
   util.request({
     url: 'http://reqspeed.test.whistlejs.com/',
+    method: 'post',
     body: util.getTextBySize(128 * 2 + 1)
   }, function(res, data) {
     now = Date.now() - now;
