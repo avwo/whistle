c5ca839a27ea55ddc962fcca9b9d9913d37458ae
avwo <avwu@qq.com>
2017-11-15 16:54:24 +0800

    feat: Send with binary

--- biz/webui/cgi-bin/socket/data.js
+++ biz/webui/cgi-bin/socket/data.js
@@ -1,10 +1,11 @@
 var events = require('../../lib/events');
 
 module.exports = function(req, res) {
-  var cId = req.body.cId;
-  var data = req.body.data;
+  var body = req.body;
+  var cId = body.cId;
+  var data = body.data;
   if (cId && data) {
-    events.emit('composer-' + cId, data);
+    events.emit('composer-' + cId, data, body.type === 'bin');
   }
   res.json({ec: 0});
 };
