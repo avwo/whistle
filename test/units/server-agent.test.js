273346c4ec378572c2c9d31433b55ce3d951da66
avwo <avwu@qq.com>
2017-11-23 23:02:11 +0800

    refactor: Add hagent

--- test/units/server-agent.test.js
+++
@@ -1,8 +0,0 @@
-// var util = require('../util.test');
-
-module.exports = function() {
-  // 容易造成ci失败
-  // for (var i = 0; i < 221; i++) {
-  //   util.request('https://' + i + '.server-agent.com/');
-  // }
-};
