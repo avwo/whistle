17eb50e1c6088a7c71795554f3f2d317acc631f3
avwo <avwu@qq.com>
2018-01-14 23:53:38 +0800

    refactor: eslint

--- biz/webui/htdocs.js
+++ biz/webui/htdocs.js
@@ -2,31 +2,31 @@ var path = require('path');
 var ROOT = path.join(__dirname, 'htdocs');
 
 function getFile(file) {
-	return path.join(ROOT, file || '');
+  return path.join(ROOT, file || '');
 }
 
 exports.getFile = getFile;
 
 function getJsFile(file) {
-	return path.join(ROOT, 'js', file || '');
+  return path.join(ROOT, 'js', file || '');
 }
 
 exports.getJsFile = getJsFile;
 
 function getHtmlFile(file) {
-	return path.join(ROOT, file || '');
+  return path.join(ROOT, file || '');
 }
 
 exports.getHtmlFile = getHtmlFile;
 
 function getImgFile(file) {
-	return path.join(ROOT, 'img', file || '');
+  return path.join(ROOT, 'img', file || '');
 }
 
 exports.getImgFile = getImgFile;
 
 function getCssFile(file) {
-	return path.join(ROOT, 'css', file || '');
+  return path.join(ROOT, 'css', file || '');
 }
 
 exports.getCssFile = getCssFile;
