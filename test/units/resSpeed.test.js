471912ff1bc5ae48c96fd93df887fca734122448
avwo <avwu@qq.com>
2018-04-13 13:31:41 +0800

    fix: set method=post

--- test/units/resSpeed.test.js
+++ test/units/resSpeed.test.js
@@ -4,7 +4,8 @@ module.exports = function() {
   var now = Date.now();
   util.request({
     url: 'http://resspeed.test.whistlejs.com/',
-    body: util.getTextBySize(128 * 2 + 1)
+    body: util.getTextBySize(128 * 2 + 1),
+    method: 'post'
   }, function(res, data) {
     now = Date.now() - now;
     now.should.above(2000);
