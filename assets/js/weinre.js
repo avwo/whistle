7d4efd229137b3660a310ff4f36e614a17767c5d
avwo <avwu@qq.com>
2018-05-27 01:33:12 +0800

    refactor: weinre

---
+++ assets/js/weinre.js
@@ -0,0 +1,17 @@
+
+(function() {
+  if (typeof window === 'undefined' || window.WeinreServerURL) {
+    return;
+  }
+  window.WeinreServerURL = '$WEINRE_PATH';
+  var head = document.head || document.getElementsByTagName('head')[0] || document.documentElement;
+  var script = document.createElement('script');
+  script.async = true;
+  script.charset = 'utf8';
+  script.src = '$WEINRE_URL';
+  if (head.firstChild) {
+    head.insertBefore(script, head.firstChild);
+  } else {
+    head.appendChild(script);
+  }
+})();
