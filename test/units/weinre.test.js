7299e834ff104230aa36a2817d384d14351470db
avwo <avwu@qq.com>
2018-05-27 11:23:04 +0800

    test: weinre

--- test/units/weinre.test.js
+++ test/units/weinre.test.js
@@ -4,8 +4,8 @@ module.exports = function() {
   util.request('http://weinre1.test2.whistlejs.com/index.html?resBody=_', function(res, body) {
     body.should.containEql('http://local.whistlejs.com/...whistle-path.5b6af7b9884e1165...///weinre.6668/target/target-script-min.js#xxx');
   });
-  util.request('http://weinre1.test.whistlejs.com/index.html?resBody=_', function(res, body) {
-    body.should.containEql('http://weinre1.test.whistlejs.com/...whistle-path.5b6af7b9884e1165...///weinre.6668/target/target-script-min.js#xxx');
+  util.request('https://weinre1.test.whistlejs.com/index.html?resBody=_', function(res, body) {
+    body.should.containEql('https://weinre1.test.whistlejs.com/...whistle-path.5b6af7b9884e1165...///weinre.6668/target/target-script-min.js#xxx');
   });
   util.request({
     url: 'http://weinre2.test.whistlejs.com/index.html',
@@ -21,8 +21,8 @@ module.exports = function() {
     body: 'sssssss'
   });
 
-  util.request('http://weinre1.test2.whistlejs.com:1234/index.html?resBody=_', function(res, body) {
-    body.should.containEql('http://local.whistlejs.com/...whistle-path.5b6af7b9884e1165...///weinre.6668/target/target-script-min.js#xxx');
+  util.request('https://weinre1.test2.whistlejs.com:1234/index.html?resBody=_', function(res, body) {
+    body.should.containEql('https://local.whistlejs.com/...whistle-path.5b6af7b9884e1165...///weinre.6668/target/target-script-min.js#xxx');
   });
   util.request('https://weinre1.test.whistlejs.com:1234/index.html?resBody=_', function(res, body) {
     body.should.containEql('https://weinre1.test.whistlejs.com:1234/...whistle-path.5b6af7b9884e1165...///weinre.6668/target/target-script-min.js#xxx');
