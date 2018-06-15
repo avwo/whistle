26dbec3ca8f8dd58cdbb21df6e49f2a7613b088f
avwo <avwu@qq.com>
2018-06-15 20:36:40 +0800

    feat: handle use cmd

--- biz/webui/cgi-bin/rules/project.js
+++ biz/webui/cgi-bin/rules/project.js
@@ -2,7 +2,15 @@ var rules = require('../../lib/rules');
 
 module.exports = function(req, res) {
   var body = req.body;
-  console.log(body)
-  // var result = rules.moveTo(body.from, body.to, body.clientId);
-  res.json({ ec: 0, rules: 1 });
+  var name = body.name;
+  if (!body.rules || !name) {
+    return res.json({ ec: 0, rules: !!rules.get(name) });
+  }
+  rules.add(name, body.rules);
+  rules.select(name);
+  var first = rules.list()[0];
+  if (first) {
+    rules.moveTo(name, first.name);
+  }
+  res.json({ ec: 0 });
 };
