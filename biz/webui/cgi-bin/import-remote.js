48088cdf324c998e8fac7440588fc2c2d4ac52bd
avwo <avwu@qq.com>
2018-06-29 09:12:42 +0800

    feat: import remote data

--- biz/webui/cgi-bin/import-remote.js
+++ biz/webui/cgi-bin/import-remote.js
@@ -11,7 +11,8 @@ module.exports = function(req, res) {
       }
       var status = response && response.statusCode;
       if (status !== 200) {
-        return res.json({ec: 2, msg: 'Request failed (Status: ' + status + ')'});
+        var msg = status > 200 && status < 400 ? 'No body data' : 'Request failed';
+        return res.json({ec: 2, msg: msg + ' (Status: ' + status + ')'});
       }
       return res.json({ec: 0, msg: body});
     });
