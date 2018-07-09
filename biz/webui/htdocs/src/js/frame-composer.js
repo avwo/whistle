4e602409cb8cd479b8ba81c3c08b2d4b3acb6f47
avwo <avwu@qq.com>
2018-07-09 22:40:45 +0800

    feat: handle the frames of socket

--- biz/webui/htdocs/src/js/frame-composer.js
+++ biz/webui/htdocs/src/js/frame-composer.js
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var dataCenter = require('./data-center');
 var util = require('./util');
 var events = require('./events');
+var fromByteArray  = require('base64-js').fromByteArray ;
 
 var MAX_FILE_SIZE = 1024 * 1025;
 var MAX_LENGTH = 1024 * 64;
@@ -26,7 +27,24 @@ var FrameComposer = React.createClass({
     var hide = util.getBoolean(this.props.hide);
     return hide != util.getBoolean(nextProps.hide) || !hide;
   },
-  selectFile: function() {
+  uploadTextToServer: function() {
+    this.target = 'server';
+    this.dataType = 'text';
+    this.dataField.click();
+  },
+  uploadBinToServer: function() {
+    this.target = 'server';
+    this.dataType = 'bin';
+    this.dataField.click();
+  },
+  uploadTextToClient: function() {
+    this.target = 'client';
+    this.dataType = 'text';
+    this.dataField.click();
+  },
+  uploadBinToClient: function() {
+    this.target = 'client';
+    this.dataType = 'bin';
     this.dataField.click();
   },
   onFormChange: function() {
@@ -34,23 +52,43 @@ var FrameComposer = React.createClass({
     this.dataField.value = '';
   },
   uploadForm: function(form) {
-    if (form.get('uploadData').size > MAX_FILE_SIZE) {
+    var file = form.get('uploadData');
+    if (file.size > MAX_FILE_SIZE) {
       return alert('The file size can not exceed 1m.');
     }
-    dataCenter.socket.upload(form);
+    var reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    var self = this;
+    var params = {
+      target: self.target,
+      type: self.dataType
+    };
+    reader.onload = function () {
+      params.base64 = fromByteArray(new window.Uint8Array(reader.result));
+      self.send(params);
+      self.dataField.value = '';
+    };
+  },
+  send: function(params, cb) {
+    var data = this.props.data;
+    if (!data) {
+      return;
+    }
+    params.reqId = data.id;
   },
   onSend: function(e) {
     var value = this.state.text;
     if (!value) {
       return;
     }
-    var self = this;
-    dataCenter.socket.send({
-      cId: this.props.cId,
-      type: e.target.nodeName === 'A' ? 'bin' : 'text/plain',
-      data: value.replace(/\r\n|\r|\n/g, '\r\n')
-    }, function(data) {
-      self.setState({ text: '' });
+    var target = e.target;
+    var params = {
+      type: target.nodeName === 'A' ? 'bin' : 'text',
+      target: target.getAttribute('data-type') ? 'server' : 'client',
+      text: value.replace(/\r\n|\r|\n/g, '\r\n')
+    };
+    this.send(params, function() {
+      
     });
   },
   setTextarea: function(text) {
@@ -70,42 +108,38 @@ var FrameComposer = React.createClass({
     var isJSON = this.state.isJSON;
     var text = this.state.text;
     var closed = data.closed;
-    var disabled = closed || data.hasPendingData;
     var tips = data.closed ? 'The connection is closed' : undefined;
     return (
       <div onDrop={this.onDrop} className={'fill orient-vertical-box w-frames-composer' + (this.props.hide ? ' hide' : '')}>
         <div className="w-frames-composer-action">
           <div className="btn-group">
-            <button disabled={disabled} title={tips} onMouseDown={this.preventDefault} onClick={this.onSend}
+            <button disabled={closed} title={tips} onMouseDown={this.preventDefault} onClick={this.onSend}
               type="button" className="btn btn-default btn-sm">Send to client</button>
-            <button disabled={disabled} title={tips} type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <button disabled={closed} title={tips} type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <span className="caret"></span>
             </button>
-            <ul className={'dropdown-menu' + (disabled ? ' hide' : '')}>
+            <ul className={'dropdown-menu' + (closed ? ' hide' : '')}>
               <li><a onClick={this.onSend} href="javascript:;">Send binary data</a></li>
-              <li><a onClick={this.selectFile} href="javascript:;">Upload text data</a></li>
-              <li><a onClick={this.selectFile} href="javascript:;">Upload binary data</a></li>
+              <li><a onClick={this.uploadTextToClient} href="javascript:;">Upload text data</a></li>
+              <li><a onClick={this.uploadBinToClient} href="javascript:;">Upload binary data</a></li>
             </ul>
           </div>
           <div className="btn-group">
-            <button disabled={disabled} title={tips} onMouseDown={this.preventDefault}
+            <button disabled={closed} title={tips} onMouseDown={this.preventDefault} data-target="server"
               onClick={this.onSend} type="button" className="btn btn-primary btn-sm">Send to server</button>
-            <button disabled={disabled} title={tips} type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <button disabled={closed} title={tips} type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <span className="caret"></span>
             </button>
-            <ul className={'dropdown-menu' + (disabled ? ' hide' : '')}>
-              <li><a onClick={this.onSend} href="javascript:;">Send binary data</a></li>
-              <li><a onClick={this.selectFile} href="javascript:;">Upload text data</a></li>
-              <li><a onClick={this.selectFile} href="javascript:;">Upload binary data</a></li>
+            <ul className={'dropdown-menu' + (closed ? ' hide' : '')}>
+              <li><a data-target="server" onClick={this.onSend} href="javascript:;">Send binary data</a></li>
+              <li><a onClick={this.uploadTextToServer} href="javascript:;">Upload text data</a></li>
+              <li><a onClick={this.uploadBinToServer} href="javascript:;">Upload binary data</a></li>
             </ul>
           </div>
           <button disabled={!isJSON} type="button" title="Format JSON" className="btn btn-default w-format-json-btn">Format</button>
         </div>
         <textarea maxLength={MAX_LENGTH} value={text} onChange={this.onTextareaChange} placeholder={'Input the text'} className="fill"></textarea>
         <form ref="uploadDataForm" method="post" encType="multipart/form-data" style={{display: 'none'}}> 
-          <input name="reqId" type="hidden" />
-          <input name="dataType" type="hidden" />
-          <input name="sendTo" type="hidden" />
           <input ref="uploadData" onChange={this.onFormChange} type="file" name="uploadData" />
         </form>
       </div>
