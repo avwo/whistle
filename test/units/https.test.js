748bfa30204a4dfe47b8bbe8b806b20ce099a7fd
avwo <avwu@qq.com>
2018-07-14 00:40:42 +0800

    test: auto2http

--- test/units/https.test.js
+++ test/units/https.test.js
@@ -2,10 +2,13 @@ var util = require('../util.test');
 var config = require('../config.test');
 
 module.exports = function() {
+  util.request('https://127.0.0.1:8080/auto2http.html', function(res, data) {
+    data.url.should.be.equal('/auto2http.html');
+  });
   util.request('https://https.w2.org/index.html', function(res, data) {
     data.body.should.be.equal('test');
   });
-  
+
   util.request('https://https1.w2.org:' + config.httpsPort + '/index.html', function(res, data) {
     data.body.should.be.equal('test');
   });
