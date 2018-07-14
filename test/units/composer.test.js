44bf7b28c38fc5f1df7d888b97c3414c62d4b02f
avwo <avwu@qq.com>
2018-07-14 23:03:01 +0800

    test: composer

---
+++ test/units/composer.test.js
@@ -0,0 +1,64 @@
+var util = require('../util.test');
+
+module.exports = function () {
+  util.request({
+    url: 'http://local.whistlejs.com/cgi-bin/composer',
+    method: 'post',
+    json: {
+      url: 'http://127.0.0.1:8080',
+      headers: 'test: 123',
+      method: 'POST',
+      body: 'test'
+    }
+  });
+  util.request({
+    url: 'http://local.whistlejs.com/cgi-bin/composer',
+    method: 'post',
+    json: {
+      url: 'http://127.0.0.1:8080',
+      headers: 'test: 123',
+      method: 'POST',
+      body: 'test'
+    }
+  });
+  util.request({
+    url: 'http://local.whistlejs.com/cgi-bin/composer',
+    method: 'post',
+    json: {
+      url: 'http://127.0.0.1:8080',
+      headers: 'test: 123',
+      method: 'POST',
+      body: 'test'
+    }
+  });
+  util.request({
+    url: 'http://local.whistlejs.com/cgi-bin/composer',
+    method: 'post',
+    json: {
+      url: 'http://127.0.0.1:8080',
+      headers: 'test: 123',
+      method: 'POST',
+      body: 'test'
+    }
+  });
+  util.request({
+    url: 'http://local.whistlejs.com/cgi-bin/composer',
+    method: 'post',
+    json: {
+      url: 'http://127.0.0.1:8080',
+      headers: 'test: 123',
+      method: 'POST',
+      body: 'test'
+    }
+  });
+  util.request({
+    url: 'http://local.whistlejs.com/cgi-bin/composer',
+    method: 'post',
+    json: {
+      url: 'http://127.0.0.1:8080',
+      headers: 'test: 123',
+      method: 'POST',
+      body: 'test'
+    }
+  });
+};
