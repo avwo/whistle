4393ebcc810cf57ba57d3bdebad6dfb816ae13cd
avwo <avwu@qq.com>
2018-07-14 23:21:53 +0800

    test: composer

--- test/units/others.test.js
+++ test/units/others.test.js
@@ -5,9 +5,6 @@ module.exports = function() {
     res.statusCode.should.be.equal(502);
   });
   require('../../biz/webui/cgi-bin/util').formatDate();
-  util.request('http://127.0.0.1:6666/_/-/abc.htm', function(res, data) {
-    data.url.should.be.equal('/abc.htm');
-  });
   util.request('http://test.internal.path.com/...whistle-path.5b6af7b9884e1165...///whistle._abc/index.html', function(res, data) {
     data.url.should.be.equal('/...whistle-path.5b6af7b9884e1165...///whistle._abc/index.html');
   });
