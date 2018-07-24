df07a439c7e1fee35903e4cd8225f1b2163ab9dc
avwo <avwu@qq.com>
2018-07-24 13:33:36 +0800

    refactor: bind host

--- biz/init.js
+++ biz/init.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var util = require('../lib/util');
 
+var LOCALHOST = '127.0.0.1';
+
 module.exports = function init(proxy, callback) {
   var config = proxy.config;
   var count = 2;
@@ -12,7 +14,11 @@ module.exports = function init(proxy, callback) {
   if (config.customUIPort) {
     var server = http.createServer();
     require(config.uipath)(server, proxy);
-    server.listen(config.uiport, execCallback);
+    if (config.host === LOCALHOST) {
+      server.listen(config.uiport, LOCALHOST, execCallback);
+    } else {
+      server.listen(config.uiport, execCallback);
+    }
   } else {
     util.getServer(function(server, port) {
       config.uiport = port;
