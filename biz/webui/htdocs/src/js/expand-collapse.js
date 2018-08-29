85dee33c158bb8742bbd56996f78df3d7ccae68f
avwo <avwu@qq.com>
2018-08-30 00:05:34 +0800

    perf: refine the perf of log & properties

--- biz/webui/htdocs/src/js/expand-collapse.js
+++ biz/webui/htdocs/src/js/expand-collapse.js
@@ -1,29 +1,37 @@
 var React = require('react');
 
+var MIN_LENGTH = 1024 * 2;
+var EXPAND_LENGTH = 1024 * 32;
+
 var ExpandCollapse = React.createClass({
   getInitialState: function() {
-    return {};
+    return { expandLength: MIN_LENGTH };
   },
   componentWillReceiveProps: function(nextProps) {
     if (nextProps.text !== this.props.text
       || this.props.wStyle !== nextProps.wStyle) {
-      this.state.expended = false;
+      this.state.expandLength = MIN_LENGTH;
     }
   },
   onExpand: function() {
-    this.setState({ expended: !this.state.expended });
+    var text = this.props.text;
+    var len = text && text.length || 0;
+    var expLen = this.state.expandLength;
+    this.setState({ expandLength: expLen >= len ? MIN_LENGTH : expLen + EXPAND_LENGTH });
   },
   render: function() {
     var text = this.props.text;
-    if (!text || text.length < 2100) {
+    var len = text && text.length || 0;
+    if (len < 2100) {
       return <span style={this.wStyle}>{text}</span>;
     }
-    var expended = this.state.expended;
+    var expandLength = this.state.expandLength;
+    var isCollapse = expandLength >= len;
     return (
       <span style={this.wStyle}>
-        {expended ? text : text.substring(0, 2050) + '...'}
+        {isCollapse ? text : text.substring(0, expandLength) + '...'}
         <button onClick={this.onExpand} className="w-expand-collapse">
-          {expended ? 'Collapse' : 'Expand'}
+          {isCollapse ? 'Collapse' : 'Expand'}
         </button>
       </span>
     );
