e76a9180d30a9eac5baa5c4ac35bcdbe293d5900
avwo <avwu@qq.com>
2018-09-13 15:06:07 +0800

    feat: refine Composer

--- biz/webui/htdocs/src/js/props-editor.js
+++ biz/webui/htdocs/src/js/props-editor.js
@@ -12,7 +12,34 @@ var PropsEditor = React.createClass({
     return {};
   },
   update: function(data) {
-    this.setState({ data: data });
+    var modal = {};
+    var overflow;
+    if (data) {
+      var keys = Object.keys(data);
+      overflow = keys.length >= MAX_COUNT;
+      if (overflow) {
+        keys = keys.slice(0, MAX_COUNT);
+      }
+      keys.forEach(function(name) {
+        var value = data[name];
+        var shortName = name.substring(0, MAX_NAME_LEN);
+        if (!Array.isArray(value)) {
+          modal[name + '_0'] =  {
+            name: shortName,
+            value: util.toString(value).substring(0, MAX_VALUE_LEN)
+          };
+          return;
+        }
+        value.forEach(function(val, i) {
+          modal[name + '_' + i] =  {
+            name: shortName,
+            value: util.toString(val).substring(0, MAX_VALUE_LEN)
+          };
+        });
+      });
+    }
+    this.setState({ modal: modal });
+    return overflow;
   },
   onEditor: function() {
     if (this.props.disabled) {
@@ -27,36 +54,28 @@ var PropsEditor = React.createClass({
 
   },
   render: function() {
-    var data = this.state.data || '';
-    var keys = Object.keys(data);
-    if (keys.length > MAX_COUNT) {
-      keys = keys.slice(0, MAX_COUNT);
-    }
+    var modal = this.state.modal || '';
+    var keys = Object.keys(modal);
+    
     return (
       <div className={'fill orient-vertical-box w-props-editor' + (this.props.hide ? ' hide' : '')}>
         {keys.length ? (<table className="table">
           <tbody>
             {
               keys.map(function(name) {
-                var value = data[name];
-                if (!Array.isArray(value)) {
-                  value = [value];
-                }
-                name = name.substring(0, MAX_NAME_LEN);
-                return value.map(function(val) {
-                  return (
-                    <tr>
-                      <th>{name}</th>
-                      <td>
-                        <pre>{util.toString(val).substring(0, MAX_VALUE_LEN)}</pre>
-                      </td>
-                      <td className="w-props-ops">
-                        <a className="glyphicon glyphicon-remove" href="javascript:;" title="Delete"></a>
-                        <a className="glyphicon glyphicon-edit" href="javascript:;" title="Edit"></a>
-                      </td>
-                    </tr>
-                  );
-                });
+                var item = modal[name];
+                return (
+                  <tr key={name}>
+                    <th>{item.name}</th>
+                    <td>
+                      <pre>{item.value}</pre>
+                    </td>
+                    <td className="w-props-ops">
+                      <a className="glyphicon glyphicon-remove" href="javascript:;" title="Delete"></a>
+                      <a className="glyphicon glyphicon-edit" href="javascript:;" title="Edit"></a>
+                    </td>
+                  </tr>
+                );
               })
             }
           </tbody>
