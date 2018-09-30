7073a19bc421b711e9a5fafa6c4b1e396388402a
avwo <avwu@qq.com>
2018-09-30 18:28:52 +0800

    test: change type

--- test/assets/values/rawFile2.js
+++ test/assets/values/rawFile2.js
@@ -1,4 +1,4 @@
 HTTP/1.1 200 OK
-content-type: text/plain
+content-type: application/javascript
 set-cookie: res_cookie_name=123; domain=.test.com; samesite=Lax; path=/; max-age=20000; expires=sxxxx; secure; httponly
 
