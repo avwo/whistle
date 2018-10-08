bb48d26a16dd39a8dfba6c4f98c5ac04a1e08397
avwo <avwu@qq.com>
2018-10-08 10:56:51 +0800

    refactor: add http-mgr

--- biz/webui/cgi-bin/import-remote.js
+++ biz/webui/cgi-bin/import-remote.js
@@ -1,15 +1,20 @@
-var parseUrl = require('url').parse;
 var util = require('../lib/util');
+
+var MAX_LEN = 1024 * 1024 * 6;
 var HTTP_RE = /https?:\/\//i;
 
 module.exports = function(req, res) {
   var url = req.query.url;
   if (HTTP_RE.test(url)) {
-    util.getResponseBody(parseUrl(url), function(err, body, response) {
+    util.request({
+      url: url,
+      maxLength: MAX_LEN
+    }, function(err, body, r) {
       if (err) {
-        return res.json({ec: 2, em: err.message});
+        var msg = err.code === 'EEXCEED'  ? 'The size of response body exceeds 6m' : err.message;
+        return res.json({ec: 2, em: msg});
       }
-      var status = response && response.statusCode;
+      var status = r && r.statusCode;
       if (status !== 200) {
         var em = status > 200 && status < 400 ? 'No body data' : 'Request failed';
         return res.json({ec: 2, em: em + ' (statusCode: ' + status + ')'});
