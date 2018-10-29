2ada1c263889ddcd58851482a553ca43c8138451
avwo <avwu@qq.com>
2018-10-29 20:36:35 +0800

    feat: custom socket data parer by plugin

--- biz/webui/cgi-bin/custom-frames.js
+++ biz/webui/cgi-bin/custom-frames.js
@@ -1,6 +1,13 @@
-var socketMgr = require('../lib/proxy').socketMgr;
+var proxy = require('../lib/proxy');
+var socketMgr = proxy.socketMgr;
 
 module.exports = function(req, res) {
-  console.log(req.body);
-  res.json({ ec: 0 });
+  var result = {};
+  req.body.idList.forEach(function(reqId) {
+    result[reqId] = socketMgr.getData(reqId);
+  });
+  req.body.frames.forEach(function(frame) {
+    proxy.emit('frame', frame);
+  });
+  res.json(result);
 };
