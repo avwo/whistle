3aa5832b0bf0792179c33ca53358c4a462f6f8e0
avwo <avwu@qq.com>
2018-11-16 20:00:34 +0800

    style: resize column width

--- biz/webui/htdocs/src/js/spinner.js
+++ biz/webui/htdocs/src/js/spinner.js
@@ -2,15 +2,21 @@ require('../css/spinner.css');
 var React = require('react');
 
 var Spinner = React.createClass({
+  onDragStart: function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  },
+  onClick: function(e) {
+    e.stopPropagation();
+  },
   render: function() {
     var order = this.props.order;
     var desc = order == 'desc';
     if (!desc && order != 'asc') {
       order = null;
     }
-
     return (
-      <div className="w-spinner">
+      <div onClick={this.onClick} onDragStart={this.onDragStart} draggable={true} className="w-spinner">
         <span className={'glyphicon glyphicon-triangle-top' + (order ? ' spinner-' + order : '')}></span>
         <span className={'glyphicon glyphicon-triangle-bottom' + (order ? ' spinner-' + order : '')}></span>
       </div>
