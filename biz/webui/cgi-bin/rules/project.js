c4ead89ef41ee1fc8f154ae13523723164749c49
avwo <avwu@qq.com>
2018-12-17 09:55:03 +0800

    refactor: supports both body.rules and body.value

--- biz/webui/cgi-bin/rules/project.js
+++ biz/webui/cgi-bin/rules/project.js
@@ -3,10 +3,11 @@ var rules = require('../../lib/rules');
 module.exports = function(req, res) {
   var body = req.body;
   var name = body.name;
-  if (!body.rules || !name) {
+  var rulesText = body.rules || body.value;
+  if (!rulesText || !name || typeof rulesText !== 'string') {
     return res.json({ ec: 0, rules: !!rules.get(name) });
   }
-  rules.add(name, body.rules);
+  rules.add(name, rulesText);
   rules.select(name);
   var first = rules.list()[0];
   if (first) {
