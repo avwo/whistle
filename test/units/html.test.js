d817c73f653374da572ecd8705625e003fa4cc68
avwo <avwu@qq.com>
2019-01-31 18:01:10 +0800

    test: htmlPrepend

--- test/units/html.test.js
+++ test/units/html.test.js
@@ -4,6 +4,9 @@ module.exports = function() {
   util.request('http://html1.test.whistlejs.com/index.html?resBody=_', function(res, body) {
     body.should.equal('_\r\nhtml');
   });
+  util.request('http://prependhtml.whistlejs.com/index.html?resBody=_', function(res, body) {
+    body.should.equal(['<!DOCTYPE html>', '-1', '-2', '1', '2', '3', '4'].join('\r\n'));
+  });
 
   util.request({
     method: 'post',
