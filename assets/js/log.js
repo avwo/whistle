2c812b125e168e19c98afbe05ab214215d25def4
avwo <avwu@qq.com>
2019-03-12 16:37:40 +0800

    refactor: add `;`

--- assets/js/log.js
+++ assets/js/log.js
@@ -1,5 +1,5 @@
 
-(function() {
+;(function() {
   if (typeof window === 'undefined' || typeof Image === 'undefined') {
     return;
   }
--- assets/js/weinre.js
+++ assets/js/weinre.js
@@ -1,5 +1,5 @@
 
-(function() {
+;(function() {
   if (typeof window === 'undefined' || window.WeinreServerURL) {
     return;
   }
