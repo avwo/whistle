95f68f1314dda0f1a04bde71c4abc7a5b629cc4b
avwo <avwu@qq.com>
2019-03-13 10:20:32 +0800

    feat: write temp value to local file

--- biz/webui/cgi-bin/rules/select.js
+++ biz/webui/cgi-bin/rules/select.js
@@ -1,8 +1,26 @@
+var fs = require('fs');
+var config = require('../../../../lib/config');
 var rules = require('../../../../lib/rules/util').rules;
+
+var TEMP_VALUES_DIR = config.TEMP_VALUES_DIR;
+var TEMP_NAME_RE = /^\d{1,30}$/;
+
+function writeTempValue(data, callback) {
+  if (!TEMP_NAME_RE.test(data.key) || typeof data.value !== 'string') {
+    return callback();
+  }
+  var filepath = TEMP_VALUES_DIR + '/' + data.key;
+  fs.writeFile(filepath, data.value, callback);
+}
 
 module.exports = function(req, res) {
   var body = req.body;
-  rules.add(body.name, body.value, body.clientId);
-  rules.select(req.body.name);
-  res.json({ec: 0, em: 'success', defaultRulesIsDisabled: rules.defaultRulesIsDisabled(), list: rules.getSelectedList()});
+  writeTempValue(body, function(err) {
+    if (err) {
+      return res.json({ec: 0, em: err.message});
+    }
+    rules.add(body.name, body.value, body.clientId);
+    rules.select(req.body.name);
+    res.json({ec: 0, em: 'success', defaultRulesIsDisabled: rules.defaultRulesIsDisabled(), list: rules.getSelectedList()});
+  });
 };
