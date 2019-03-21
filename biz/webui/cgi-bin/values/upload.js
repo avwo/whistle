8b0c3171adff81cda47f62c3dfa21bffa5a70e50
avwo <avwu@qq.com>
2019-03-22 00:51:40 +0800

    feat: add upload cgi

---
+++ biz/webui/cgi-bin/values/upload.js
@@ -0,0 +1,7 @@
+var values = require('../../../../lib/rules/util').values;
+
+module.exports = function(req, res) {
+  var body = req.body;
+  values.add(body.name, body.value, body.clientId);
+  res.json({ec: 0, em: 'success'});
+};
