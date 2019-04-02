549c5e02ac29478bc46c7401c58d305ad4355694
avwo <avwu@qq.com>
2019-04-02 20:38:22 +0800

    feat: support for uploading files

--- biz/webui/cgi-bin/values/check-file.js
+++ biz/webui/cgi-bin/values/check-file.js
@@ -4,11 +4,10 @@ var LIMIMT_FILES_COUNT = values.LIMIMT_FILES_COUNT;
 
 module.exports = function (req, res) {
   var files = values.getUploadFiles();
-  var name = req.body.name;
   var count = parseInt(req.body.count, 10) || 0;
   var len = files.length;
   var isMax = len >= LIMIMT_FILES_COUNT && count < LIMIMT_FILES_COUNT;
-  var exists = name && files.indexOf(name) !== -1;
+  var exists = values.existsFile(req.body.name);
   res.json({
     ec: 0,
     em: 'success',
