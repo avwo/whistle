2381bd3cc555bc8bc8a85586c167b2353d6aa684
avwo <avwu@qq.com>
2019-04-12 11:52:27 +0800

    feat: add enable://multiClient

--- biz/index.js
+++ biz/index.js
@@ -91,7 +91,7 @@ module.exports = function(req, res, next) {
       var colon = proxyUrl.indexOf(':');
       var proxyPort = colon === -1 ? 80 : proxyUrl.substring(colon + 1);
       req.headers.host = 'local.whistlejs.com';
-      util.setClientId(req.headers, rules.resolveEnable(req));
+      util.setClientId(req.headers, rules.resolveEnable(req), req.clientIp);
       util.transformReq(req, res, proxyPort > 0 ? proxyPort : 80, ip);
     });
   } else if (isWebUI) {
