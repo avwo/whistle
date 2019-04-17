6e72202d7806fb225d19819b4b4be4452ab3ccb2
avwo <avwu@qq.com>
2019-04-17 23:09:41 +0800

    style: add Copy button

--- biz/webui/htdocs/src/js/properties.js
+++ biz/webui/htdocs/src/js/properties.js
@@ -2,6 +2,7 @@ require('./base-css.js');
 require('../css/properties.css');
 var React = require('react');
 var util = require('./util');
+var CopyBtn = require('./copy-btn');
 var ExpandCollapse = require('./expand-collapse');
 
 var Properties = React.createClass({
@@ -13,12 +14,12 @@ var Properties = React.createClass({
   },
   render: function() {
     var props = this.props;
-    var enableViewSource = props.enableViewSource;
+    var sourceText = props.enableViewSource;
     var viewSource = this.state.viewSource;
     var modal = props.modal || {};
     var title = props.title || {};
     var keys = Object.keys(modal);
-    var sourceText = enableViewSource && keys.map(function(name) {
+    sourceText = sourceText && keys.map(function(name) {
       var value = modal[name];
       return (Array.isArray(value) ?
           value.map(function(val, i) {
@@ -27,11 +28,17 @@ var Properties = React.createClass({
           : name + ': ' + util.toString(value)
       );
     }).join('\n');
+
     return (
       <div className={ 'w-properties-wrap ' + (viewSource ? 'w-properties-view-source ' : 'w-properties-view-parsed ') + (props.hide ? 'hide' : '') }>
-        { enableViewSource ? <a onClick={this.toggle} className="w-properties-btn">{ viewSource ? 'ViewParsed' : 'ViewSource' }</a> : undefined }
-        { enableViewSource ? (<pre className="w-properties-source">
-          {sourceText && sourceText.length >= 2100 ? <ExpandCollapse text={sourceText} /> : sourceText}
+        { sourceText ? 
+          <div className="w-textarea-bar">
+            <CopyBtn value={sourceText} />
+            <a onClick={this.toggle} href="javascript:;">{ viewSource ? 'ViewParsed' : 'ViewSource' }</a>
+          </div> : undefined
+        }
+        { sourceText ? (<pre className="w-properties-source">
+        { sourceText.length >= 2100 ? <ExpandCollapse text={sourceText} /> : sourceText}
         </pre>) : undefined }
         <table className={'table w-properties w-properties-parsed ' + (props.className || '')}>
           <tbody>
