17b7798bef04224c5390a95f66d0330aa3d65a11
avwo <avwu@qq.com>
2019-04-17 23:47:21 +0800

    refactor: refine code

--- app.js
+++
@@ -1,6 +0,0 @@
-var colors = require('colors/safe');
-
-/*eslint no-console: "off"*/
-require('./index')(function() {
-  console.log(colors.green('whistle is listening on ' + this.address().port + '.'));
-});
