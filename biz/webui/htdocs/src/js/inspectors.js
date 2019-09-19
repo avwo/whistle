8c2c86e1a7bda12e67960567e84d65babe963e87
avwo <avwu@qq.com>
2019-09-19 14:30:10 +0800

    style: show Url in inspectors panel

--- biz/webui/htdocs/src/js/inspectors.js
+++ biz/webui/htdocs/src/js/inspectors.js
@@ -3,6 +3,7 @@ var React = require('react');
 var Divider = require('./divider');
 var ReqDetail = require('./req-detail');
 var ResDetail = require('./res-detail');
+var ExpandCollapse = require('./expand-collapse');
 var util = require('./util');
 
 var Inspector = React.createClass({
@@ -25,22 +26,29 @@ var Inspector = React.createClass({
   render: function() {
     var props = this.props;
     var modal = props.modal;
+    var url = modal && modal.url;
     this.endTime = modal && (modal.endTime || modal.lost);
     return (
-      <Divider vertical="true" className={'w-detail-inspectors' + (props.hide ? ' hide' : '')}>
-        <div className="fill orient-vertical-box">
-          <div className="w-detail-inspectors-title" title={modal && modal.url}>
-            <span className="glyphicon glyphicon-arrow-right"></span>Request
-          </div>
-          <ReqDetail modal={modal} />
+      <div className={'fill orient-vertical-box w-detail-inspectors' + (util.getBoolean(this.props.hide) ? ' hide' : '')}>
+        <div className="box w-detail-inspectors-url" title={url}>
+          <label>Url</label>
+          <div className="fill"><ExpandCollapse text={url} /></div>
         </div>
-        <div className="fill orient-vertical-box">
-          <div className="w-detail-inspectors-title">
-          <span className="glyphicon glyphicon-arrow-left"></span>Response
+        <Divider vertical="true">
+          <div className="fill orient-vertical-box">
+            <div className="w-detail-inspectors-title">
+              <span className="glyphicon glyphicon-arrow-right"></span>Request
+            </div>
+            <ReqDetail modal={modal} />
           </div>
-          <ResDetail modal={modal} />
-        </div>
-      </Divider>
+          <div className="fill orient-vertical-box">
+            <div className="w-detail-inspectors-title">
+            <span className="glyphicon glyphicon-arrow-left"></span>Response
+            </div>
+            <ResDetail modal={modal} />
+          </div>
+        </Divider>
+      </div>
     );
   }
 });
