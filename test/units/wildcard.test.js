8890c78c2ef88b3ba5ebd0c7940ee53d0c7b605d
avwo <avwu@qq.com>
2019-10-20 20:11:23 +0800

    test: wildcard pattern `.test.com`

--- test/units/wildcard.test.js
+++ test/units/wildcard.test.js
@@ -1,4 +1,5 @@
 var util = require('../util.test');
+var assert = require('assert');
 
 module.exports = function() {
   util.request('https://wildcard.cn/abc/index.html', function(res, data) {
@@ -76,5 +77,34 @@ module.exports = function() {
     body: 'sssssss'
   }, function(res, data) {
     data.name.should.be.equal('https');
+  });
+
+  util.request('https://test.wildcard5.com/abc/index.html', function(res, data) {
+    data.list[10].should.be.equal('a\rb\nc');
+    data.list[20].should.be.equal(123);
+    data.list[30].should.be.equal('123');
+    data.list[40].a.should.be.equal('123');
+  });
+
+  util.request('https://test.wildcard5.com/wildcard5/_____/abc/index.html?test=123', function(res, data) {
+    data.list[10].should.be.equal('a\rb\nc');
+    data.list[20].should.be.equal(123);
+    data.list[30].should.be.equal('123');
+    data.list[40].a.should.be.equal('123');
+    data.ec.should.be.equal('abc/index.html');
+  });
+
+  util.request('https://w1.test.wildcard5.com/abc/index.html', function(res, data) {
+    data.list[10].should.be.equal('a\rb\nc');
+    data.list[20].should.be.equal(123);
+    data.list[30].should.be.equal('123');
+    data.list[40].a.should.be.equal('123');
+  });
+  util.request({
+    url: 'https://w2.w1.test.wildcard5.com/abc/index.html',
+    method: 'post',
+    body: 'sssssss'
+  }, function(res, data) {
+    assert(data.list === undefined);
   });
 };
