0414b54546d8e6e977dd58ae705826825cbe27c1
avwo <avwu@qq.com>
2019-12-10 13:49:58 +0800

    style: refine ipv6 highlight

--- biz/webui/htdocs/src/js/rules-mode.js
+++ biz/webui/htdocs/src/js/rules-mode.js
@@ -5,6 +5,11 @@ var forwardRules = protocols.getForwardRules();
 var pluginRules = protocols.getPluginRules();
 var DOT_PATTERN_RE = /^\.[\w-]+(?:[?$]|$)/;
 var DOT_DOMAIN_RE = /^\.[^./?]+\.[^/?]/;
+var IPV4_RE = /^(?:::(?:ffff:)?)?(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)?$/;
+var IPV4_PORT_RE = /^(?:::(?:ffff:)?)?(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(?:\:\d+)?$/;
+var FULL_IPV6_RE = /^[\da-f]{1,4}(?::[\da-f]{1,4}){7}$/;
+var SHORT_IPV6_RE = /^[\da-f]{1,4}(?::[\da-f]{1,4}){0,6}$/;
+var IP_WITH_PORT_RE = /^\[([:\da-f.]+)\](?::(\d+))?$/i;
 
 events.on('updatePlugins', function() {
   forwardRules = protocols.getForwardRules();
@@ -14,8 +19,30 @@ events.on('updatePlugins', function() {
 
 CodeMirror.defineMode('rules', function() {
   function isIP(str) {
-    return /^(?:::(?:ffff:)?)?(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(?:\:\d+)?$/.test(str)
-      || /^[:\da-f]*:[\da-f]*:[\da-f]+(?:::)?$/i.test(str) || /^\[[:\da-f]*:[\da-f]*:[\da-f]+(?:::)?\](?::\d+)?$/i.test(str);
+    var ipv4Test = IPV4_PORT_RE;
+    if (IP_WITH_PORT_RE.test(str)) {
+      str = RegExp.$1;
+      var port = RegExp.$2;
+      if (port) {
+        if (port == 0 || port > 65535) {
+          return false;
+        }
+        ipv4Test = IPV4_RE;
+      }
+    }
+    if (ipv4Test.test(str)) {
+      return true;
+    }
+    var index = str.indexOf('::');
+    if (index !== -1) {
+      if (str === '::' || str.indexOf('::', index + 1) !== -1) {
+        return false;
+      }
+      str = str.split('::', 2);
+      str = str[0] && str[1] ? str.join(':') : (str[0] || str[1]);
+      return SHORT_IPV6_RE.test(str);
+    }
+    return FULL_IPV6_RE.test(str);
   }
   function isHost(str) {
     return /^x?hosts?:\/\//.test(str);
