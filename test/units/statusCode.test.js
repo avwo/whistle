fcd1e878786c7e528b54200ae6ae5b10addeb599
avwo <avwu@qq.com>
2020-05-14 23:46:29 +0800

    test: statusCode

--- test/units/statusCode.test.js
+++ test/units/statusCode.test.js
@@ -2,18 +2,18 @@ var util = require('../util.test');
 
 module.exports = function() {
   util.request({
-    url: 'http://statuscode1.test.whistlejs.com/index.html?resBody=',
+    url: 'http://statuscode1.testx.whistlejs.com/index.html?resBody=',
     method: 'POST',
     body: 'xxxxxxxxxxxx'
   }, function(res) {
     res.statusCode.should.equal(999);
   });
 
-  util.request('https://statuscode2.test.whistlejs.com/index.html?resBody=', function(res) {
+  util.request('https://statuscode2.testx.whistlejs.com/index.html?resBody=', function(res) {
     res.statusCode.should.equal(101);
   });
 
-  util.request('http://statuscode3.test.whistlejs.com/index.html?resBody=', function(res) {
+  util.request('http://statuscode3.testx.whistlejs.com/index.html?resBody=', function(res) {
     res.statusCode.should.equal(500);
   });
 
