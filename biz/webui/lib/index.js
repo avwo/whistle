e67e392f0d43fff62b479238c694329a9860108b
avwo <avwu@qq.com>
2020-07-15 00:02:55 +0800

    refactor: add login status check to /cgi-bin/composer

--- biz/webui/lib/index.js
+++ biz/webui/lib/index.js
@@ -24,7 +24,7 @@ var uploadJsonParser = bodyParser.json(UPLOAD_PARSE_CONF);
 var GET_METHOD_RE = /^get$/i;
 var WEINRE_RE = /^\/weinre\/.*/;
 var ALLOW_PLUGIN_PATHS = ['/cgi-bin/rules/list2', '/cgi-bin/values/list2', '/cgi-bin/get-custom-certs-info'];
-var DONT_CHECK_PATHS = ['/cgi-bin/server-info', '/cgi-bin/plugins/is-enable', '/cgi-bin/composer', '/cgi-bin/socket/data',
+var DONT_CHECK_PATHS = ['/cgi-bin/server-info', '/cgi-bin/plugins/is-enable', '/cgi-bin/socket/data',
                         '/preview.html', '/cgi-bin/socket/abort', '/cgi-bin/socket/change-status',
                         '/cgi-bin/sessions/export', '/cgi-bin/sessions/import', '/cgi-bin/rootca', '/cgi-bin/log/set'];
 var PLUGIN_PATH_RE = /^\/(whistle|plugin)\.([^/?#]+)(\/)?/;
