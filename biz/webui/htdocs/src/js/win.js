d99b8a9cac78ebf4da8d585726623432611c5f58
avwo <avwu@qq.com>
2021-08-10 11:41:45 +0800

    style: use custom confirm dialog

--- biz/webui/htdocs/src/js/win.js
+++ biz/webui/htdocs/src/js/win.js
@@ -2,6 +2,7 @@ var $ = require('jquery');
 
 var alertDialog;
 var confirmDialog;
+var handleConfirm;
 
 function createAlert() {
   if (!alertDialog) {
@@ -30,12 +31,24 @@ function createConfirm() {
                               '<pre></pre>' +
                             '</div>' +
                             '<div class="modal-footer">' +
-                              '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>' +
-                              '<button type="button" class="btn btn-primary">Confirm</button>' +
+                              '<button type="button" class="btn btn-default w-win-cancel" data-dismiss="modal">Cancel</button>' +
+                              '<button type="button" class="btn btn-primary w-win-confirm" data-dismiss="modal">Confirm</button>' +
                             '</div>' +
                           '</div>' +
                         '</div>' +
                       '</div>');
+    confirmDialog.on('click', '.w-win-cancel', function() {
+      if (typeof handleConfirm === 'function') {
+        handleConfirm(false);
+      }
+      handleConfirm = null;
+    });
+    confirmDialog.on('click', '.w-win-confirm', function() {
+      if (typeof handleConfirm === 'function') {
+        handleConfirm(true);
+      }
+      handleConfirm = null;
+    });
   }
   return confirmDialog;
 }
@@ -47,7 +60,13 @@ function mockAlert(msg) {
 }
 
 function mockConfirm(msg, cb) {
-  cb(window.confirm(msg));
+  if (handleConfirm) {
+    return;
+  }
+  handleConfirm = cb;
+  createConfirm();
+  confirmDialog.find('pre').text(msg);
+  confirmDialog.modal('show');
 }
 
 exports.alert = mockAlert;
