4ecc856847b701a18afa9663b3be47e27443441d
avwo <avwu@qq.com>
2021-09-11 03:05:44 +0800

    refactor: refine code

--- biz/webui/cgi-bin/socket/abort.js
+++ biz/webui/cgi-bin/socket/abort.js
@@ -1,6 +1,10 @@
-var socketMgr = require('../../lib/proxy').socketMgr;
+var proxy = require('../../lib/proxy');
+
+var socketMgr = proxy.socketMgr;
 
 module.exports = function(req, res) {
+  var reqId = req.body.reqId;
+  proxy.abortRequest(reqId);
   socketMgr.abort(req.body.reqId);
   res.json({ec: 0});
 };
