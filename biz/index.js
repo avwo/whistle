39502db148cb277eba786f13176753a638dd0ffd
avwo <avwu@qq.com>
2021-09-18 21:31:37 +0800

    feat: add proxyServer mode

--- biz/index.js
+++ biz/index.js
@@ -73,7 +73,7 @@ module.exports = function(req, res, next) {
       } else if (!req.headers[config.WEBUI_HEAD]) {
         isWebUI = false;
       }
-      if (isProxyReq && !config.isLocalUIUrl(host)) {
+      if (!config.proxyServer && isProxyReq && !config.isLocalUIUrl(host)) {
         isWebUI = false;
         req.isPluginReq = true;
         req._isProxyReq = true;
