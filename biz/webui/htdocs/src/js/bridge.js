b9da1b8612d57c51034e35ec77dda648ab16a7ee
avwo <avwu@qq.com>
2021-12-23 13:13:50 +0800

    style: refine code

---
+++ biz/webui/htdocs/src/js/bridge.js
@@ -0,0 +1,79 @@
+var qrCode = require('qrcode');
+var $ = require('jquery');
+var message = require('./message');
+var createCgi = require('./cgi').createCgi;
+var dataCenter = require('./data-center');
+var util = require('./util');
+var modal = require('./modal');
+var events = require('./events');
+var mockWin = require('./win');
+
+function compatAjax(options) {
+  if (typeof options !== 'string') {
+    options.type = options.type || options.method;
+  }
+  return options;
+}
+
+function getBridge() {
+  return {
+    importSessions: dataCenter.importAnySessions,
+    msgBox: message,
+    qrCode: qrCode,
+    qrcode: qrCode,
+    alert: mockWin.alert,
+    confirm: mockWin.confirm,
+    request: function(options, cb) {
+      var request = createCgi(compatAjax(options));
+      return request(options.data, cb);
+    },
+    createRequest: function(options) {
+      return createCgi(compatAjax(options));
+    },
+    showModal: modal.show,
+    getServerInfo: function() {
+      var serverInfo = dataCenter.getServerInfo();
+      return serverInfo && $.extend(true, {}, serverInfo);
+    },
+    importRules: function(data) {
+      if (!data) {
+        return;
+      }
+      var list = util.parseImportData(data, dataCenter.rulesModal);
+      var handleImport = function(sure) {
+        if (sure) {
+          data = {};
+          list.forEach(function(item) {
+            data[item.name] = item.value;
+          });
+          events.trigger('uploadRules', data);
+        }
+      };
+      if (!list.hasConflict) {
+        return handleImport(true);
+      }
+      mockWin.confirm('Conflict with existing content, whether to continue to overwrite them?', handleImport);
+    },
+    importValues: function(data) {
+      if (!data) {
+        return;
+      }
+      var list = util.parseImportData(data, dataCenter.valuesModal, true);
+      var handleImport = function(sure) {
+        if (sure) {
+          data = {};
+          list.forEach(function(item) {
+            data[item.name] = item.value;
+          });
+          events.trigger('uploadValues', data);
+        }
+      };
+      if (!list.hasConflict) {
+        return handleImport(true);
+      }
+      mockWin.confirm('Conflict with existing content, whether to continue to overwrite them?', handleImport);
+    }
+  };
+}
+
+module.exports = getBridge;
--- biz/webui/htdocs/src/js/iframes.js
+++ biz/webui/htdocs/src/js/iframes.js
@@ -1,12 +1,6 @@
-var qrCode = require('qrcode');
-var $ = require('jquery');
-var message = require('./message');
-var createCgi = require('./cgi').createCgi;
-var dataCenter = require('./data-center');
 var util = require('./util');
 var modal = require('./modal');
-var events = require('./events');
-var mockWin = require('./win');
+var getBridge = require('./bridge');
 
 var MAX_COUNT = 6;
 var MAX_CACHE_TIME = 1000 * 60 * 3;
@@ -57,13 +51,6 @@ function getItem(win) {
   } catch (e) {}
 }
 
-function compatAjax(options) {
-  if (typeof options !== 'string') {
-    options.type = options.type || options.method;
-  }
-  return options;
-}
-
 function onPluginContextMenuReady(win) {
   var item = getItem(win);
   if (!item || item.emit) {
@@ -77,77 +64,21 @@ function onPluginContextMenuReady(win) {
     } catch (e) {}
   }
   try {
-    win.initWhistleBridge({
-      importSessions: dataCenter.importAnySessions,
-      msgBox: message,
-      qrCode: qrCode,
-      qrcode: qrCode,
-      alert: mockWin.alert,
-      confirm: mockWin.confirm,
-      request: function(options, cb) {
-        var request = createCgi(compatAjax(options));
-        return request(options.data, cb);
-      },
-      createRequest: function(options) {
-        return createCgi(compatAjax(options));
-      },
-      showModal: modal.show,
-      getServerInfo: function() {
-        var serverInfo = dataCenter.getServerInfo();
-        return serverInfo && $.extend(true, {}, serverInfo);
-      },
-      createModal: function(options) {
-        var dialog = modal.create(options);
-        var hide = dialog.hide;
-        dialog.hide = function(_destroy) {
-          if (_destroy) {
-            var index = item.dialogs.indexOf(dialog);
-            index !== -1 && item.dialogs.splice(index, 1);
-          }
-          hide(_destroy);
-        };
-        item.dialogs.push(dialog);
-        return dialog;
-      },
-      importRules: function(data) {
-        if (!data) {
-          return;
+    var bridge = getBridge();
+    bridge.createModal = function(options) {
+      var dialog = modal.create(options);
+      var hide = dialog.hide;
+      dialog.hide = function(_destroy) {
+        if (_destroy) {
+          var index = item.dialogs.indexOf(dialog);
+          index !== -1 && item.dialogs.splice(index, 1);
         }
-        var list = util.parseImportData(data, dataCenter.rulesModal);
-        var handleImport = function(sure) {
-          if (sure) {
-            data = {};
-            list.forEach(function(item) {
-              data[item.name] = item.value;
-            });
-            events.trigger('uploadRules', data);
-          }
-        };
-        if (!list.hasConflict) {
-          return handleImport(true);
-        }
-        mockWin.confirm('Conflict with existing content, whether to continue to overwrite them?', handleImport);
-      },
-      importValues: function(data) {
-        if (!data) {
-          return;
-        }
-        var list = util.parseImportData(data, dataCenter.valuesModal, true);
-        var handleImport = function(sure) {
-          if (sure) {
-            data = {};
-            list.forEach(function(item) {
-              data[item.name] = item.value;
-            });
-            events.trigger('uploadValues', data);
-          }
-        };
-        if (!list.hasConflict) {
-          return handleImport(true);
-        }
-        mockWin.confirm('Conflict with existing content, whether to continue to overwrite them?', handleImport);
-      }
-    });
+        hide(_destroy);
+      };
+      item.dialogs.push(dialog);
+      return dialog;
+    };
+    win.initWhistleBridge(bridge);
   } catch (e) {}
   item.list.forEach(emit);
   item.emit = emit;
