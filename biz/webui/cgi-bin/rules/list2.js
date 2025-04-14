053925eaced547de1c58c5f39eddf224621ab838
avwo <avwu@qq.com>
2025-04-14 20:30:00 +0800

    feat: keep order

--- biz/webui/cgi-bin/rules/list2.js
+++ biz/webui/cgi-bin/rules/list2.js
@@ -2,11 +2,25 @@ var get = require('./index');
 
 module.exports = function(req, res) {
   var rules = get();
-  var data = {
-    Default: rules.defaultRules
-  };
-  rules.list.forEach(function(item) {
-    data[item.name] = item.data;
-  });
+  var data;
+  if (req.query.order) {
+    data = [{
+      name: 'Default',
+      value: rules.defaultRules
+    }];
+    rules.list.forEach(function(item) {
+      data.push({
+        name: item.name,
+        value: item.data
+      });
+    });
+  } else {
+    data = {
+      Default: rules.defaultRules
+    };
+    rules.list.forEach(function(item) {
+      data[item.name] = item.data;
+    });
+  }
   res.json(data);
 };
--- biz/webui/cgi-bin/values/list2.js
+++ biz/webui/cgi-bin/values/list2.js
@@ -1,9 +1,20 @@
 var get = require('./index');
 
 module.exports = function(req, res) {
-  var data = {};
-  get().list.forEach(function(item) {
-    data[item.name] = item.data;
-  });
+  var data;
+  if (req.query.order) {
+    data = [];
+    get().list.forEach(function(item) {
+      data.push({
+        name: item.name,
+        value: item.data
+      });
+    });
+  } else {
+    data = {};
+    get().list.forEach(function(item) {
+      data[item.name] = item.data;
+    });
+  }
   res.json(data);
 };
