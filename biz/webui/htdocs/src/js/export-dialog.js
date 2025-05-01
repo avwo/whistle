723c73b8178b1096f1b49c637d3e678dc75c9000
avwo <avwu@qq.com>
2025-05-01 11:09:56 +0800

    feat: import & export dialog

---
+++ biz/webui/htdocs/src/js/export-dialog.js
@@ -0,0 +1,127 @@
+require('../css/import-dialog.css');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Dialog = require('./dialog');
+var ShareViaUrlBtn = require('./share-via-url-btn');
+var util = require('./util');
+
+var ExportDialog = React.createClass({
+  getInitialState: function () {
+    return {};
+  },
+  show: function (name, data) {
+    var self = this;
+    self.refs.exportDialog.show();
+    setTimeout(function () {
+      var input = ReactDOM.findDOMNode(self.refs.input);
+      input.focus();
+      input.select();
+    }, 500);
+    name = name || 'network';
+    self.setState({
+      name: name,
+      title: util.getDialogTitle(name, 'Export'),
+      showOptions: name === 'network',
+      data: data
+    });
+  },
+  getFilename: function () {
+    var name = this.state.name;
+    var suffix = name === 'console' || name === 'server' ? '.log' : '.txt';
+    var filename = ReactDOM.findDOMNode(this.refs.input).value.trim();
+    if (filename) {
+      if (!/\.(txt|json)/i.test(filename)) {
+        filename += suffix;
+      }
+      return filename;
+    }
+    switch (name) {
+    case 'networkSettings':
+      filename = 'network_settings_';
+      break;
+    case 'composer':
+      filename = 'composer_';
+      break;
+    case 'console':
+      filename = 'console_';
+      break;
+    case 'server':
+      filename = 'server_';
+      break;
+    case 'rulesSettings':
+      filename = 'rules_settings_';
+      break;
+    case 'valuesSettings':
+      filename = 'values_settings_';
+      break;
+    case 'mock':
+      filename = 'mock_';
+      break;
+    default:
+      filename = 'network_';
+    }
+    return filename + util.formatDate() + suffix;
+  },
+  hide: function () {
+    this.refs.exportDialog.hide();
+  },
+  export: function(e) {
+    if (e && e.type !== 'click' && e.keyCode !== 13) {
+      return;
+    }
+    var data = this.state.data;
+    if (typeof data === 'function') {
+      data = data();
+    }
+    this.hide();
+    util.download(data, this.getFilename());
+  },
+  onShare: function() {
+    this.hide();
+  },
+  render: function () {
+    var state = this.state;
+    var showOptions = state.showOptions;
+
+    return (
+      <Dialog ref="exportDialog" wstyle={'w-ie-dialog' + (showOptions ? ' w-export-network' : '')}>
+        <div className="modal-header">
+          <h4>{state.title}</h4>
+          <button type="button" className="close" data-dismiss="modal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <input
+            ref="input"
+            onKeyDown={this.export}
+            placeholder="Enter filename (optional)"
+            className="form-control"
+            maxLength="64"
+          />
+        </div>
+        <div className="modal-footer">
+          <button
+            type="button"
+            className="btn btn-default"
+            data-dismiss="modal"
+          >
+            Cancel
+          </button>
+          <ShareViaUrlBtn data={state.data} onComplete={this.onShare} />
+          <button
+            type="button"
+            className="btn btn-primary w-fmt-btn"
+             data-dismiss="modal"
+             onClick={this.export}
+          >
+            <span className="glyphicon glyphicon-export" />
+            Export
+          </button>
+        </div>
+      </Dialog>
+    );
+  }
+});
+
+module.exports = ExportDialog;
---
+++ biz/webui/htdocs/src/js/import-dialog.js
@@ -0,0 +1,217 @@
+require('../css/import-dialog.css');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Dialog = require('./dialog');
+var dataCenter = require('./data-center');
+var util = require('./util');
+var events = require('./events');
+var $ = require('jquery');
+var message = require('./message');
+var EditorDialog = require('./editor-dialog');
+const parseCurl = require('./parse-curl');
+
+function getAccept(name) {
+  if (name === 'network') {
+    return '.txt,.json,.saz,.har';
+  }
+  if (name === 'console' || name === 'server') {
+    return '.log';
+  }
+  return '.txt,.json';
+}
+
+function checkUrl(url) {
+  url = url.trim();
+  if (!url) {
+    message.error('The url or file path is required.');
+    return;
+  }
+  return url;
+}
+
+function parseJson(text) {
+  if (text[0] !== '{' && text[0] !== '[') {
+    return;
+  }
+  try {
+    return JSON.parse(text);
+  } catch(e) {}
+}
+
+var ImportDialog = React.createClass({
+  getInitialState: function () {
+    return {};
+  },
+  show: function (name) {
+    var self = this;
+    self.refs.importDialog.show();
+    setTimeout(function () {
+      var input = ReactDOM.findDOMNode(self.refs.input);
+      input.focus();
+      input.select();
+    }, 500);
+    name = name || 'network';
+    self.setState({
+      name: name,
+      accept: getAccept(name),
+      importCURL: name === 'composer',
+      title: util.getDialogTitle(name)
+    });
+  },
+  hide: function () {
+    this.refs.importDialog.hide();
+  },
+  showService: function () {
+    util.showService(this.state.name + 'Data');
+  },
+  importRemoteUrl: function (e) {
+    if (e && e.type !== 'click' && e.keyCode !== 13) {
+      return;
+    }
+    var self = this;
+    var input = ReactDOM.findDOMNode(self.refs.input);
+    var url = checkUrl(input.value);
+    if (!url) {
+      return;
+    }
+    dataCenter.getRemoteData(url, function (_, data) {
+      if (data) {
+        self.hide();
+        events.trigger(self.state.name + 'ImportData', [data]);
+      }
+    });
+  },
+  handleFile: function(file) {
+    if (file) {
+      this.hide();
+      events.trigger(this.state.name + 'ImportFile', [file]);
+    }
+  },
+  componentDidMount: function () {
+    var self = this;
+    self.dialogElem = $(ReactDOM.findDOMNode(self.refs.importDialog));
+    events.on('importFile', function (_, file) {
+      self.handleFile(file);
+    });
+  },
+  shouldComponentUpdate: function () {
+    return this.dialogElem.has('.in');
+  },
+  uploadFile: function() {
+    var fileInput = ReactDOM.findDOMNode(this.refs.importFile);
+    this.handleFile(fileInput.files[0]);
+    ReactDOM.findDOMNode(this.refs.importFile).value = '';
+  },
+  selectFile: function() {
+    ReactDOM.findDOMNode(this.refs.importFile).click();
+  },
+  importCURL: function(text) {
+    text = text.trim();
+    if (!text) {
+      message.error('The text is required.');
+      return false;
+    }
+    try {
+      var result = parseJson(text) || parseCurl(text);
+      if (!result || !result.url) {
+        message.error('Not CURL text.');
+        return false;
+      }
+      result.isHexText = false;
+      result.headers = util.objectToString(result.headers);
+      events.trigger('setComposerData', [result]);
+    } catch (e) {
+      message.error(e.message);
+      return false;
+    }
+
+  },
+  showImportCURL: function() {
+    this.refs.editorDialog.show();
+  },
+  render: function () {
+    var state = this.state;
+
+    return (
+      <Dialog ref="importDialog" wstyle="w-ie-dialog w-import-dialog">
+        <div className="modal-header">
+          <h4>{state.title}</h4>
+          <button type="button" className="close" data-dismiss="modal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <input
+            ref="input"
+            maxLength="2048"
+            onKeyDown={this.importRemoteUrl}
+            placeholder="Enter url or file path"
+          />
+        </div>
+        <div className="modal-footer">
+          <button
+            type="button"
+            className="btn btn-default"
+            data-dismiss="modal"
+          >
+            Cancel
+          </button>
+          {
+            state.importCURL ? <button
+            type="button"
+            className="btn btn-default"
+            data-dismiss="modal"
+            onClick={this.showImportCURL}
+          >
+            <span className="glyphicon glyphicon-file" />
+            Import CURL
+          </button> : null
+          }
+          {dataCenter.hasToken ? <button
+            type="button"
+            className="btn btn-warning"
+            data-dismiss="modal"
+            onClick={this.showService}
+          >
+            <span className="glyphicon glyphicon-cloud" />
+            Select Service Data
+          </button> : null}
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={this.selectFile}
+          >
+            <span className="glyphicon glyphicon-folder-open" />
+            Select File
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary w-fmt-btn"
+            onMouseDown={util.preventBlur}
+            onClick={this.importRemoteUrl}
+          >
+            <span className="glyphicon glyphicon-import" />
+            Import
+          </button>
+        </div>
+        <form
+          ref="importFileForm"
+          encType="multipart/form-data"
+          style={{ display: 'none' }}
+        >
+          <input
+            ref="importFile"
+            onChange={this.uploadFile}
+            name="fileInput"
+            type="file"
+            accept={state.accept}
+          />
+        </form>
+        <EditorDialog ref="editorDialog" title="Import CURL" hideFormat="1" placeholder="Enter CURL text"
+          textEditor onConfirm={this.importCURL} />
+      </Dialog>
+    );
+  }
+});
+
+module.exports = ImportDialog;
