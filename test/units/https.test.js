ecb13a19bf89e35abac93b26ee2f71af442ead23
avwo <avwu@qq.com>
2025-11-09 23:00:34 +0800

    test: update node

--- test/units/https.test.js
+++ test/units/https.test.js
@@ -2,7 +2,7 @@ var util = require('../util.test');
 var config = require('../config.test');
 
 module.exports = function() {
-  util.request('https://127.0.0.1:8080/auto2http.html', function(res, data) {
+  util.request('http://127.0.0.1:8080/auto2http.html', function(res, data) {
     data.url.should.be.equal('/auto2http.html');
   });
   util.request('https://https.w2.org/index.html', function(res, data) {
@@ -12,19 +12,19 @@ module.exports = function() {
   util.request('https://https1.w2.org:' + config.httpsPort + '/index.html', function(res, data) {
     data.body.should.be.equal('test');
   });
-  
+
   util.request('http://https1.w2.org:' + config.serverPort + '/index.html', function(res, data) {
     data.type.should.be.equal('server');
   });
-  
+
   util.request('https://https2.w2.org:' + config.httpsPort + '/index.html', function(res, data) {
     data.body.should.be.equal('test');
   });
-  
+
   util.request('http://https2.w2.org/index.html', function(res, data) {
     data.type.should.be.equal('server');
   });
-  
+
   util.request('https://https3.w2.org:5566/index.html', function(res, data) {
     data.body.should.be.equal('test');
   });
